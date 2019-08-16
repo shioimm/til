d51f5a88a766111ddfaab4023d79cf75a3fa11ab
Misaki Shioi <shioi.mm@gmail.com>
2019-08-16 14:14:19 +0900

    Add quick_sort

---
+++ books/grokking_algorithm/quick_sort.rb
@@ -0,0 +1,12 @@
+def quick_sort(arr)
+  if arr.length < 2
+    arr
+  else
+    pivot = arr.first
+    smaller = arr.select { |a| a < pivot }
+    bigger = arr.select { |a| a > pivot }
+    quick_sort(smaller) + [pivot] + quick_sort(bigger)
+  end
+end
+
+p quick_sort(Array(1..10).shuffle)
