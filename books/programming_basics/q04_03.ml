27c67b89f559173fc2fdecb713a1ff69ed4d7aa6
Misaki Shioi <shioi.mm@gmail.com>
2019-08-25 23:16:39 +0900

    Add q04_03

---
+++ books/programming_basics/q04_03.ml
@@ -0,0 +1,6 @@
+(* 問題4-3 from 浅井健一 著「プログラミングの基礎」 *)
+
+let normal_weight m = m ** 2.0 *. 22.0 ;;
+(* val normal_weight : float -> float = <fun> *)
+
+(* # normal_weight 1.54 ;; => - : float = 52.1752 *)
