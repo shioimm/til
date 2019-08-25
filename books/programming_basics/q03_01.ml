a4438c78e5ae680c0ad02e6b57fd07d1a123395f
Misaki Shioi <shioi.mm@gmail.com>
2019-08-25 22:26:42 +0900

    Add q03_01

---
+++ books/programming_basics/q03_01.ml
@@ -0,0 +1,13 @@
+(* 問題3-1 from 浅井健一 著「プログラミングの基礎」 *)
+
+let e = 2.7182 ;;
+(* val e : float = 2.7182 *)
+
+let positive = 2.7183;;
+(* val positive : float = 2.7183 *)
+
+let seconds_of_day = 60 * 60 * 24 ;;
+(* val seconds_of_day : int = 86400 *)
+
+let name = "茗荷谷" ;;
+(* val name : string = "茗荷谷" *)
