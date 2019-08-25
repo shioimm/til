b52c998d3ffeca9e9ec65f4efe28fcaad61a16e1
Misaki Shioi <shioi.mm@gmail.com>
2019-08-25 23:20:02 +0900

    Add q04_04

---
+++ books/programming_basics/q04_04.ml
@@ -0,0 +1,6 @@
+(* 問題4-4 from 浅井健一 著「プログラミングの基礎」 *)
+
+let bmi m kg = kg /. (m ** 2.0) ;;
+(* val bmi : float -> float -> float = <fun> *)
+
+(* # bmi 1.84 70.0 ;; => - : float = 20.6758034026465 *)
