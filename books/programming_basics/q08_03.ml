7da849cfae978db30bc76588387c73b0a0ae4956
Misaki Shioi <shioi.mm@gmail.com>
2019-08-31 17:28:49 +0900

    Add q9_02 & Modify q08_03

--- books/programming_basics/q08_03.ml
+++ books/programming_basics/q08_03.ml
@@ -9,7 +9,7 @@ type person_t = {
   blood_type: string;
 }
 
-{
+let suzuki = {
   name="susuki tamako";
   m=158;
   kg=50;
@@ -17,6 +17,21 @@ type person_t = {
   date=31;
   blood_type="O"
 }
-(* - : person_t =
-{name = "susuki tamako"; m = 158; kg = 50; month = 12; date = 31;
- blood_type = "O"} *)
+
+let yamada = {
+  name="yamada otoko";
+  m=176;
+  kg=68;
+  month=1;
+  date=1;
+  blood_type="A"
+}
+
+(*
+val suzuki : person_t =
+  {name = "susuki tamako"; m = 158; kg = 50; month = 12; date = 31;
+   blood_type = "O"}
+val yamada : person_t =
+  {name = "yamada otoko"; m = 176; kg = 68; month = 1; date = 1;
+   blood_type = "A"}
+*)
---
+++ books/programming_basics/q09_02.ml
@@ -0,0 +1,12 @@
+(* 問題9-2 from 浅井健一 著「プログラミングの基礎」 *)
+
+#use "q08_03.ml" ;;
+
+suzuki :: yamada :: [] ;;
+
+(*
+[{name = "susuki tamako"; m = 158; kg = 50; month = 12; date = 31;
+  blood_type = "O"};
+ {name = "yamada otoko"; m = 176; kg = 68; month = 1; date = 1;
+  blood_type = "A"}]
+*)
