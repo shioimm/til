8a632ed2ded5de6997355302539e045189929fa9
Misaki Shioi <shioi.mm@gmail.com>
2019-09-05 21:18:47 +0900

    Add q10_10 & q10_11 & q10_12

---
+++ books/programming_basics/q10_10.ml
@@ -0,0 +1,17 @@
+(* 問題10-10 from 浅井健一 著「プログラミングの基礎」 *)
+
+#use "q09_09.ml"
+
+(* 目的：ローマ字の駅名を漢字に直す *)
+(* roman_to_name : string -> station_t list -> string *)
+let rec roman_to_name rm0 lst = match lst with
+    [] -> ""
+  | { name=n; kana=k; roman=rm1; route=rt } :: rest ->
+      if rm0 = rm1
+      then n
+      else roman_to_name rm0 rest
+
+(* test *)
+let test1 = roman_to_name "myogadani" global_station_list = "茗荷谷"
+let test2 = roman_to_name "shibuya" global_station_list = "渋谷"
+let test3 = roman_to_name "otemachi" global_station_list = "大手町"
---
+++ books/programming_basics/q10_12.ml
@@ -0,0 +1,25 @@
+(* 問題10-11 from 浅井健一 著「プログラミングの基礎」 *)
+
+#use "q10_10.ml"
+#use "q10_11.ml"
+
+(* 目的：ふたつの駅の間の距離を文字列で表現する *)
+(* display_distance : string string -> string *)
+let display_distance st1 st2 =
+  let name1 = roman_to_name st1 global_station_list in
+  if name1 = ""
+  then st1 ^ " という駅は存在しません"
+  else let name2 = roman_to_name st2 global_station_list in
+    if name2 = ""
+    then st2 ^ " という駅は存在しません"
+    else let distance = get_distance st1 st2 global_between_list in
+      if distance = infinity
+      then st1 ^ "と" ^ st2 "はつながっていません"
+      else st1 ^ "から" ^ st2 "までは" ^ string_of_float distance ^ "です"
+
+(* test *)
+let test1 = display_distance "myougadani" "shinotsuka" = "myougadani という駅は存在しません"
+let test1 = display_distance "myogadani" "shinotsuka" = "茗荷谷から新大塚までは 1.2 キロです"
+let test1 = display_distance "myogadani" "ikebukuro" = "茗荷谷と池袋はつながっていません"
+let test1 = display_distance "tokyo" "ootemachi" = "ootemachi という駅は存在しません"
+let test1 = display_distance "tokyo" "otemachi"= "東京から大手町までは 0.6 キロです"
