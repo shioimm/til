023782717ad16320cf4d90c7b0e7cc68d5854295
Misaki Shioi <shioi.mm@gmail.com>
2019-09-08 11:11:47 +0900

    Add programs with natural number and factorial
    11. 自然数と再帰

---
+++ books/programming_basics/fac.ml
@@ -0,0 +1,17 @@
+(* from 浅井健一 著「プログラミングの基礎」 *)
+
+(* 目的：自然数 n の階乗を求める *)
+(* fac : int -> int *)
+
+let rec fac n =
+  if n = 0
+  then 1
+  else n * fac (n - 1)
+
+(* test *)
+let test1 = fac 0 = 1
+let test2 = fac 1 = 1
+let test3 = fac 2 = 2
+let test4 = fac 3 = 6
+let test5 = fac 4 = 24
+let test6 = fac 10 = 3628800
