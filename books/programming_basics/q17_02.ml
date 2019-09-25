f8aebeaf581002663ec519b268670f9b05cdb97a
Misaki Shioi <shioi.mm@gmail.com>
2019-09-25 19:48:17 +0900

    Add q17_02 & q17_03.ml & q17_04.ml

---
+++ books/programming_basics/q17_02.ml
@@ -0,0 +1,14 @@
+(* 問題17-2 from 浅井健一 著「プログラミングの基礎」 *)
+
+type year_t = January of int
+            | February of int
+            | March of int
+            | April of int
+            | May of int
+            | June of int
+            | July of int
+            | August of int
+            | September of int
+            | October of int
+            | November of int
+            | December of int
