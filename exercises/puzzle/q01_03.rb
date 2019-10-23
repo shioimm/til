c11ea2f25a165e64182357d8b8f5cdb0f4929cbc
Misaki Shioi <shioi.mm@gmail.com>
2019-10-23 10:37:55 +0900

    Refactor q01_03.rb

--- exercises/puzzle/q01_03.rb
+++ exercises/puzzle/q01_03.rb
@@ -1,9 +1,5 @@
 # Q03 from プログラマ脳を鍛える数学パズル シンプルで高速なコードが書けるようになる70問
 
-RANGE = 1..100
-
-RANGE.each do |i|
-  flag = false
-  RANGE.each { |ii| flag = !flag if i.modulo(ii).zero? }
-  p i if flag
+(1..100).each do |i|
+  pp i if (1..i).select { |n| (i % n).zero? }.size.odd?
 end
