6bc2dee6892a8e99cef40659e8cef3c5cb68fa50
Misaki Shioi <shioi.mm@gmail.com>
2019-12-24 18:31:09 +0900

    Add 03_tcp_server_with_while

---
+++ ruby/network_programming/03_tcp_server_with_while/server.rb
@@ -0,0 +1,24 @@
+# 引用元: rubyネットワークプログラミング / 何度も受信できるTCPサーバ
+# http://www.geekpage.jp/programming/ruby-network/tcp-3.php
+
+require 'socket'
+
+s0 = TCPServer.open(20000)
+
+# セッションをループさせる
+while true
+  ## クライアントからの接続要求を受け入れてセッションを開始
+  ## -> 文字列を送信する
+  ## -> セッションを終了する
+
+  sock = s0.accept
+
+  while buf = sock.gets
+    p buf
+  end
+
+  sock.close
+end
+
+# 終了条件をつけていないため、ソケットが閉じられることはない
+s0.close
