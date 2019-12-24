1c2f56b3287a299af7edc6d9d502f46aadf7e249
Misaki Shioi <shioi.mm@gmail.com>
2019-12-24 18:52:31 +0900

    Add 04_tcp_server_with_accepted_client

---
+++ ruby/network_programming/04_tcp_server_with_accepted_client/server.rb
@@ -0,0 +1,23 @@
+# 引用元: rubyネットワークプログラミング / TCPサーバ(acceptした相手の確認)
+# http://www.geekpage.jp/programming/ruby-network/accept-from.php
+# 参照: https://docs.ruby-lang.org/ja/2.6.0/class/IPSocket.html
+
+require 'socket'
+
+s0 = TCPServer.open(20000)
+
+while true
+  sock = s0.accept
+
+  # peeraddrはIPSocketのインスタンスメソッド
+  # 接続先ソケットの情報を表す配列を返す
+  p sock.peeraddr
+
+  while buf = sock.gets
+    p buf
+  end
+
+  sock.close
+end
+
+s0.close
