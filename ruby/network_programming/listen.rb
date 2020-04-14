5d938202e74c37a30699a3beec1d95fd5c88f665
Misaki Shioi <shioi.mm@gmail.com>
2020-04-14 21:45:25 +0900

    Update network_programming

---
+++ ruby/network_programming/listen.rb
@@ -0,0 +1,13 @@
+# 引用: Working with TCP Sockets (Jesse Storimer)
+# Server Lifecycle
+
+require 'socket'
+
+socket = Socket.new(:INET, :STREAM)
+addr = Socket.park_sockaddr_in(4481, '0.0.0.0')
+socket.bind(addr)
+
+# クライアントからの接続をlistenする
+# 引数 -> ソケットが許容する保留中のコネクションの最大数(listen queue)
+# (queueがあふれた場合はErrno::ECONNREFUSED / queueの最大設定可能数はマシンによる)
+socket.listen(5)
