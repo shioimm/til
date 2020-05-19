cf9ca8ab55efb5d72396ad1256342756ee8e0924
Misaki Shioi <shioi.mm@gmail.com>
2020-05-19 22:25:12 +0900

    Add exercises/c/

---
+++ exercises/c/01.c
@@ -0,0 +1,53 @@
+/*
+ * 納得C言語 [第5回]演習問題Ⅰ & [第8回]演習問題Ⅱ
+ * http://www.isl.ne.jp/pcsp/beginC/C_Language_05.html
+ * http://www.isl.ne.jp/pcsp/beginC/C_Language_08.html
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int kind, n_1, n_2, answer;
+
+  while (1) {
+    puts("Choose kind.");
+    puts("+ -> 1 | - -> 2 | * -> 3 | / -> 4 | exit -> 0");
+    scanf("%i", &kind);
+
+    if (kind == 0) {
+      puts("Exit");
+      break;
+    } else if (kind != 1 && kind != 2 && kind != 3 && kind != 4) {
+      puts("Incorrect kind.");
+      continue;
+    }
+
+    puts("Input number twice");
+    printf("a = ");
+    scanf("%i", &n_1);
+    printf("b = ");
+    scanf("%i", &n_2);
+
+    switch (kind) {
+      case 1:
+        answer = n_1 + n_2;
+        printf("%i + %i = %i\n", n_1, n_2, answer);
+        break;
+      case 2:
+        answer = n_1 - n_2;
+        printf("%i - %i = %i\n", n_1, n_2, answer);
+        break;
+      case 3:
+        answer = n_1 * n_2;
+        printf("%i * %i = %i\n", n_1, n_2, answer);
+        break;
+      case 4:
+        answer = n_1 / n_2;
+        printf("%i / %i = %i\n", n_1, n_2, answer);
+        break;
+    }
+  }
+
+  return 0;
+}
---
+++ exercises/c/02.c
@@ -0,0 +1,42 @@
+/*
+ * 納得C言語 [第8回]演習問題Ⅱ
+ * http://www.isl.ne.jp/pcsp/beginC/C_Language_08.html
+*/
+
+#include <stdio.h>
+
+int compare(int correct, int answer)
+{
+  if (answer > correct) {
+    puts("It's bigger than correct number.");
+    return 1;
+  } else if (answer < correct) {
+    puts("It's smaller than correct number.");
+    return 1;
+  } else {
+    puts("It's a correct number.");
+    return 0;
+  }
+}
+
+int main()
+{
+  int correct, count, answer;
+
+  printf("Input number of answer -> ");
+  scanf("%i", &correct);
+
+
+  for (count = 0; count < 3; count++) {
+    printf("What number did you input? -> ");
+    scanf("%i", &answer);
+
+    if (compare(correct, answer) == 0) {
+      break;
+    } else if (count == 2) {
+      printf("The correct number is %i\n", correct);
+    }
+  }
+
+  return 0;
+}
