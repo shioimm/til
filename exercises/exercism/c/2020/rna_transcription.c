027bb0037b2abb17d83e187c0d4bc9e086b162ad
Misaki Shioi <shioi.mm@gmail.com>
2020-05-31 12:45:59 +0900

    Add rna_transcription

---
+++ exercises/exercism/c/2020/rna_transcription.c
@@ -0,0 +1,29 @@
+#include "rna_transcription.h"
+#include <string.h>
+#include <stdlib.h>
+
+char *to_rna(const char *dna)
+{
+  char *rna = malloc(sizeof(char) * strlen(dna));
+
+  for (int i = 0; dna[i] != '\0'; i++) {
+    switch(dna[i]) {
+      case 'G':
+        rna[i] = 'C';
+        break;
+      case 'C':
+        rna[i] = 'G';
+        break;
+      case 'T':
+        rna[i] = 'A';
+        break;
+      case 'A':
+        rna[i] = 'U';
+        break;
+      default:
+        return NULL;
+    }
+  }
+
+  return rna;
+}
