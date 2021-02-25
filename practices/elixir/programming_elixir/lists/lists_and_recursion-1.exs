5cd0906fbdcf0a837f6fafb6facda724eb1f12a3
Misaki Shioi <shioi.mm@gmail.com>
2021-02-25 22:24:52 +0900

    Update programming_elixir/lists/

--- practices/elixir/programming_elixir/lists/lists_and_recursion-1.exs
+++ practices/elixir/programming_elixir/lists/lists_and_recursion-1.exs
@@ -1,9 +1,13 @@
 # プログラミングElixir 7.5 ListsAndRecursion-1
 
 defmodule MyList do
-  def mapsum([], func) do
+  def mapsum([], _) do
+    0
   end
 
-  def mapsum([], func) do
+  def mapsum([ head | tail ], func) do
+    func.(head) + mapsum(tail, func)
   end
 end
+
+IO.puts MyList.mapsum [1, 2, 3], &(&1 * &1)
--- practices/elixir/programming_elixir/lists/lists_and_recursion-3.exs
+++ practices/elixir/programming_elixir/lists/lists_and_recursion-3.exs
@@ -1,9 +1,20 @@
 # プログラミングElixir 7.5 ListsAndRecursion-3
 
 defmodule MyList do
-  def ceasar([], n) do
+  def ceasar([s], n)
+  when s + n > 26 do
+    s + n - 26
   end
 
-  def ceasar([], n) do
+  def ceasar([s], n)
+  when s + n <= 26 do
+    s + n
+  end
+
+  def ceasar([ head | tail ], n) do
+    [ ceasar([ head ], n) | ceasar(tail, n) ]
   end
 end
+
+IO.inspect MyList.ceasar 'abcd', 2
+IO.inspect MyList.ceasar 'ryvkve', 13
