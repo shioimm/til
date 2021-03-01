367a6c0782cba5886ae34197e3713ee12b974fbf
Misaki Shioi <shioi.mm@gmail.com>
2021-03-01 20:07:21 +0900

    Update producer_consumer/ractor.rb

--- practices/ruby/concurrency/producer_consumer/ractor.rb
+++ practices/ruby/concurrency/producer_consumer/ractor.rb
@@ -1,36 +1,27 @@
 class Producer
-  @@id = 0
-
-  def initialize(name, factory)
-    Ractor.current.name = name
+  def initialize(factory)
+    @id = 0
     @factory = factory
   end
 
   def make
     loop do
       sleep rand
-      product = "Product no. #{next_id} by #{Ractor.current.name}"
+      product = "Product no. #{@id} by #{Ractor.current.name}"
       @factory.send product
       puts "#{Ractor.current.name} makes #{product}."
     end
   end
-
-  private
-
-    def next_id
-      @@id += 1
-    end
 end
 
 class Consumer
-  def initialize(name, factory)
-    Ractor.current.name = name
+  def initialize(factory)
     @factory = factory
   end
 
   def take
     loop do
-      product = @factory.take
+      product, _ = @factory.take
       puts "#{Ractor.current.name} takes #{product}"
       sleep rand
     end
@@ -45,14 +36,14 @@ factory = Ractor.new do
 end
 
 producers = 3.times.map { |i|
-  Ractor.new do
-    Producer.new("P-#{i + 1}", factory).make
+  Ractor.new(factory, name: "P-#{i + 1}") do |factory|
+    Producer.new(factory).make
   end
 }
 
 consumers = 3.times.map { |i|
-  Ractor.new do
-    Consumer.new("C-#{i + 1}", factory).take
+  Ractor.new(factory, name: "C-#{i + 1}") do |factory|
+    Consumer.new(factory).take
   end
 }
 
