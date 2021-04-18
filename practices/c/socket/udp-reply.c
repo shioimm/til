c973c66a09314bff332521188eccc85dd1d44d35
Misaki Shioi <shioi.mm@gmail.com>
2021-04-18 20:36:08 +0900

    Update c/socket/

---
+++ practices/c/socket/udp-reply.c
@@ -0,0 +1,43 @@
+// Software Design 2021年5月号 ハンズオンTCP/IP
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main()
+{
+  int sock;
+  struct sockaddr_in addr;
+  struct sockaddr_in senderinfo;
+  socklen_t addrlen;
+  char buf[2048];
+  char ipaddrstr[INET_ADDRSTRLEN];
+  int n;
+
+  sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+  addr.sin_family      = AF_INET;
+  addr.sin_port        = htons(54321);
+  addr.sin_addr.s_addr = INADDR_ANY;
+
+  bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+
+  memset(buf, 0, sizeof(buf));
+
+  addrlen = sizeof(senderinfo);
+
+  n = recvfrom(sock, buf, sizeof(buf) - 1, 0, (struct sockaddr *)&senderinfo, &addrlen);
+
+  inet_ntop(AF_INET, &senderinfo.sin_addr, ipaddrstr, sizeof(ipaddrstr));
+  printf("UDP packet from : %s, port=%d\n", ipaddrstr, ntohs(senderinfo.sin_port));
+
+  sendto(sock, "REPLY", 6, 0, (struct sockaddr *)&senderinfo, addrlen);
+  write(fileno(stdout), buf, n);
+
+  close(sock);
+
+  return 0;
+}
