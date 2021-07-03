03ea05a4f20f61adafd6e90c234df6d833ad0940
Misaki Shioi <shioi.mm@gmail.com>
2021-07-03 11:49:58 +0900

    Add post method to Sinatra app

--- activities/20210630_rubykaigi_proposal/app/app.rb
+++ activities/20210630_rubykaigi_proposal/app/app.rb
@@ -5,5 +5,12 @@ require_relative '../server'
 
 get '/posts' do
   @posts = params[:user_id] ? Post.where(user_id: params[:user_id]) : Post.all
+
   erb :index
+end
+
+post '/posts' do
+  Post.new(user_id: params[:user_id], body: params[:body])
+
+  redirect to('/posts')
 end
