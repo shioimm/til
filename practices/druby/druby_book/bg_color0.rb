30a641cb00ab8ae616e770d232e4f01323c9da32
Misaki Shioi <shioi.mm@gmail.com>
2021-09-17 08:04:41 +0900

    Update druby/druby_book/

---
+++ practices/druby/druby_book/bg_color0.rb
@@ -0,0 +1,15 @@
+# 参照: dRubyによる分散・Webプログラミング
+class BGColor
+  def initialize
+    @colors = ['#eeeeff', 'bbbbff']
+    @count = -1
+  end
+  attr_accessor :colors
+
+  def next_bgcolor
+    @count += 1
+    @count = 0 if @colors.size <= @count
+    "bgcolor='#{@colors[@count]}'"
+  end
+  alias :to_s, :next_bgcolor
+end
