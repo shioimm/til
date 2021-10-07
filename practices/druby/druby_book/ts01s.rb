ccdfa63a64c53d4217e2c0e7537aee8306cee11a
Misaki Shioi <shioi.mm@gmail.com>
2021-10-08 08:11:15 +0900

    Update druby/druby_book/

--- practices/druby/druby_book/ts01s.rb
+++ practices/druby/druby_book/ts01s.rb
@@ -1,9 +1,34 @@
 # 参照: dRubyによる分散・Webプログラミング
 
-# $ irb --prompt simple -r drb/drb -r rinda/tuplespace
-# >> $ts = Rinda::TupleSpace.new
-# >> DRb.start_service('druby://localhost:12345', $ts)
-# >> $ts.write(['fact', 1, 5])
-# => #<DRb::DRbObject:0x00007ff0e4a22678 @ref=200, @uri="druby://localhost:12345">
-# >> res = $ts.take(['fact-answer', 1, 5, nil]) ブロック
-# => ["fact-answer", 1, 5, 120] ブロック解除
+# $ irb --prompt simple -r drb/drb
+# >> DRb.start_service
+# >> $ts = DRbObject.new_with_uri('druby://localhost:12345')
+# >> tmp, m, n = $ts.take(['fact', Integer, Integer])
+# => ["fact", 1, 5]
+# >> value = (m..n).inject(1) { |a, b| a * b }
+# => 120
+# >> $ts.write(['fact-answer', m, n, value])
+# => #<DRb::DRbObject:0x00007f9e8b1abfb8 @ref=220, @uri="druby://localhost:12345">
+
+require 'drb/drb'
+
+class FactServer
+  def initialize(ts)
+    @ts = ts
+  end
+
+  def main_loop
+    loop do
+      tuple = @ts.take(['fact', Integer, Integer])
+      m = tuple[1]
+      n = tuple[2]
+      value = (m..n).inject(1) { |a, b| a * b }
+      @ts.write(['fact-answer', m, n, value])
+    end
+  end
+end
+
+ts_uri = ARGV.shift || 'druby://localhost:12345'
+DRb.start_service
+$ts = DRbObject.new_with_uri(ts_uri)
+FactServer.new($ts).main_loop
