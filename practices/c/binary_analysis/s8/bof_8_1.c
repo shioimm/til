b40920f5441416c83996ea2bd9a4fdf18499c9f4
Misaki Shioi <shioi.mm@gmail.com>
2021-10-09 20:54:14 +0900

    Add binary_analysis/s8/

---
+++ practices/c/binary_analysis/s8/bof_8_1.c
@@ -0,0 +1,22 @@
+// はじめて学ぶバイナリ解析 不正なコード型コンピュータを守るサイバーセキュリティ技術
+#include <stdio.h>
+
+void pwn() {
+  printf("hacked!");
+}
+
+void vuln() {
+  char overflowme[48];
+
+  scanf("%[^\n]", overflowme);
+}
+
+int main() {
+  vuln();
+  printf("failed!");
+  return 0;
+}
+
+// $ gcc -m32 -fno-stack-protector bof_8_1.c
+// $ sudo sysctl -w
+// kernel.randomize_va_space=0 ASLRをOFFにする
