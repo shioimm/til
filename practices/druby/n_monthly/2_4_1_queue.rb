83b5367039a3f8782dba6bf602024f42db7325b8
Misaki Shioi <shioi.mm@gmail.com>
2021-10-27 07:50:27 +0900

    Update druby/n_monthly/

--- practices/druby/n_monthly/2_4_1_queue.rb
+++ practices/druby/n_monthly/2_4_1_queue.rb
@@ -1,2 +1,7 @@
 # n月刊ラムダノートVol2No1(2020) dRuby で楽しむ分散オブジェクト
 require 'drb'
+
+queue = Queue.new
+DRb.start_service('druby://localhost:54321', queue)
+puts DRb.uri
+sleep
--- practices/druby/n_monthly/2_4_2_consumer.rb
+++ practices/druby/n_monthly/2_4_2_consumer.rb
@@ -1,2 +1,18 @@
 # n月刊ラムダノートVol2No1(2020) dRuby で楽しむ分散オブジェクト
 require 'drb'
+
+DRb.start_service
+queue = DRbObject.new_with_uri('druby://localhost:54321')
+puts 'pop'
+p queue.pop
+
+sleep 5
+
+puts 'pop'
+p queue.pop
+puts 'pop'
+p queue.pop
+puts 'pop'
+p queue.pop
+puts 'pop'
+p queue.pop
