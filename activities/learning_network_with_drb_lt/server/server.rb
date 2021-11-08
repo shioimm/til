86d103221e6b2787cb5dec7ec7f99bb861a3a46e
Misaki Shioi <shioi.mm@gmail.com>
2021-11-08 21:28:37 +0900

    Update activities/learning_network_with_drb_lt/

--- activities/learning_network_with_drb_lt/server/server.rb
+++ activities/learning_network_with_drb_lt/server/server.rb
@@ -1,6 +1,6 @@
 require 'drb'
 
-DRb.start_service('druby://:8080', {})
+DRb.start_service("druby://#{ENV['LOCAL_HOST_ADDRESS']}:8080", {})
 puts "Start server on #{DRb.uri}"
 
 sleep 1 until DRb.front['stdout']
