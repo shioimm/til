bcc6c9744ebb61902614631ac158a377a7c5600b
Misaki Shioi <shioi.mm@gmail.com>
2021-11-14 21:35:53 +0900

    Update activities/learning_network_with_drb/

---
+++ activities/learning_network_with_drb/server/thinkpad.rb
@@ -0,0 +1,24 @@
+require 'drb'
+
+kvs      = {}
+last_kvs = {}
+
+DRb.start_service("druby://#{ENV['LOCAL_HOST_ADDRESS']}:8080", kvs)
+puts "Start server on #{DRb.uri}"
+
+loop do
+  if kvs.size > last_kvs.size
+    puts "New record has been added:\n#{kvs}"
+
+    new_values = (kvs.values - last_kvs.values)
+    new_stdouts = new_values.select { |value| value.respond_to?(:tty?) && value.tty? }
+
+    unless new_stdouts.empty?
+      new_stdouts.each { |new_stdout| new_stdout.puts "Hello from ThinkPad" }
+    end
+
+    last_kvs = kvs.dup
+  end
+
+  sleep 1
+end
--- activities/learning_network_with_drb_lt/server/server.rb
+++
@@ -1,7 +0,0 @@
-require 'drb'
-
-DRb.start_service("druby://#{ENV['LOCAL_HOST_ADDRESS']}:8080", {})
-puts "Start server on #{DRb.uri}"
-
-sleep 1 until DRb.front['stdout']
-DRb.front['stdout'].puts "Hello from server"
