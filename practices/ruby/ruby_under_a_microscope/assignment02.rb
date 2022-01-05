496bea7d7e1899cc147c7a0fa5dee290b228169c
Misaki Shioi <shioi.mm@gmail.com>
2022-01-05 18:08:59 +0900

    Update ruby/ruby_under_a_microscope/

---
+++ practices/ruby/ruby_under_a_microscope/assignment02.rb
@@ -0,0 +1,9 @@
+x = 'a'
+
+# == disasm: #<ISeq:<compiled>@<compiled>:1 (1,0)-(1,7)> (catch: FALSE)
+# local table (size: 1, argc: 0 [opts: 0, rest: -1, post: 0, block: -1, kw: -1@-1, kwrest: -1])
+# [ 1] x@0
+# 0000 putstring                              "a"                       (   1)[Li]
+# 0002 dup
+# 0003 setlocal_WC_0                          x@0
+# 0005 leave
