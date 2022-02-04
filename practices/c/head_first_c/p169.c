4a7f6ed69c215df3b033a6cedbe8369247177ea5
Misaki Shioi <shioi.mm@gmail.com>
2022-02-04 19:11:54 +0900

    Update practices/c/head_first_c/

---
+++ practices/c/head_first_c/p169.c
@@ -0,0 +1,31 @@
+// Head First C P169
+
+#include <stdio.h>
+#include "p169.h"
+
+float total = 0.0;
+short count = 0;
+short tax_percent = 6;
+
+int main(int argc, char *argv[])
+{
+  float val;
+  printf("品目の値段: ");
+
+  while (scanf("%f", &val) == 1) {
+    printf("ここまでの合計: %.2f\n", add_with_tax(val));
+    printf("品目の値段: ");
+  }
+
+  printf("\n最終合計: %.2f\n", total);
+  printf("品目数: %hi\n", count);
+
+  return 0;
+}
+
+float add_with_tax(float f) {
+  float tax_rate = 1 + tax_percent / 100.0;
+  total = total + (f * tax_rate);
+  count = count + 1;
+  return total;
+}
