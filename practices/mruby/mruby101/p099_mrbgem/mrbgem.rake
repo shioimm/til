87f2a57f7f9ebf0c053c4f4e3392297ef097d09e
Misaki Shioi <shioi.mm@gmail.com>
2022-02-25 18:05:00 +0900

    Update mruby/mruby101/

---
+++ practices/mruby/mruby101/p099_mrbgem/mrbgem.rake
@@ -0,0 +1,5 @@
+MRuby::Gem::Specification.new('mruby-hello2') do |spec|
+  spec.license = 'MIT'
+  spec.authors = 'Misaki Shioi'
+  spec.summary = 'say hello'
+end
---
+++ practices/mruby/mruby101/p099_mrbgem/src/hello.c
@@ -0,0 +1,45 @@
+#include <mruby.h>
+#include <mruby/variable.h>
+#include <mruby/string.h>
+
+const char *PUNCT_MARK[] = { "!", "?", ":)" };
+const int PUNCT_SIZE = sizeof(PUNCT_MARK) / sizeof(char*);
+
+static mrb_value mrb_mruby_hello_initialize(mrb_state *mrb, mrb_value self)
+{
+  mrb_int n;
+  mrb_value name;
+  mrb_get_args(mrb, "Si", &name, &n);
+
+  if (n < 0 || n >= PUNCT_SIZE) {
+    mrb_raisef(mrb, E_ARGUMENT_ERROR, "invalid argument.");
+  }
+
+  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@name"), name);
+  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@num"), mrb_fixnum_value(n));
+
+  return self;
+}
+
+static mrb_value mrb_mruby_greeting(mrb_state *mrb, mrb_value self)
+{
+  mrb_value name = mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@name"));
+  mrb_int n = mrb_fixnum(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@num")));
+  mrb_value hello = mrb_str_new_lit(mrb, "Hello, ");
+
+  mrb_str_cat_str(mrb, hello, name);
+  mrb_str_cat_str(mrb, hello, mrb_str_new_cstr(mrb, PUNCT_MARK[n]));
+
+  return hello;
+}
+
+void mrb_mruby_hello2_gem_init(mrb_state *mrb)
+{
+  struct RClass *cls = mrb_define_class(mrb, "Hello", mrb->object_class);
+  mrb_define_method(mrb, cls, "initialize", mrb_mruby_hello_initialize, MRB_ARGS_REQ(2));
+  mrb_define_method(mrb, cls, "greeting", mrb_mruby_greeting, MRB_ARGS_NONE());
+}
+
+void mrb_mruby_hello2_gem_final(mrb_state *mrb)
+{
+}
