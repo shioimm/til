69be98f0659b00b03e66e5513483a37963344bb4
Misaki Shioi <shioi.mm@gmail.com>
2022-03-24 09:30:46 +0900

    Upddate mruby/plugin

--- practices/mruby/plugin/main.c
+++ practices/mruby/plugin/main.c
@@ -13,9 +13,15 @@ static mrb_value mrb_plugin_init(mrb_state *mrb, mrb_value self)
   mrb_sym   protocol;
   mrb_int   port;
   mrb_get_args(mrb, "Sni", &name, &protocol, &port);
-  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@name"),     name);
-  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@protocol"), mrb_symbol_value(protocol));
-  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@port"),     mrb_int_value(mrb, port));
+
+  if ((protocol != mrb_intern_lit(mrb, "tcp")) && (protocol != mrb_intern_lit(mrb, "udp"))) {
+    mrb_raisef(mrb, E_ARGUMENT_ERROR, "invalid protocol: %S", mrb_symbol_value(protocol));
+  }
+
+  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@name"),        name);
+  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@filter_name"), mrb_funcall(mrb, name, "downcase", 0));
+  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@protocol"),    mrb_symbol_value(protocol));
+  mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@port"),        mrb_int_value(mrb, port));
 
   return self;
 }
@@ -23,6 +29,11 @@ static mrb_value mrb_plugin_init(mrb_state *mrb, mrb_value self)
 static mrb_value mrb_plugin_get_name(mrb_state *mrb, mrb_value self)
 {
   return mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@name"));
+}
+
+static mrb_value mrb_plugin_get_filter_name(mrb_state *mrb, mrb_value self)
+{
+  return mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@filter_name"));
 }
 
 static mrb_value mrb_plugin_get_protocol(mrb_state *mrb, mrb_value self)
@@ -40,10 +51,11 @@ int main()
   mrb_state *mrb = mrb_open();
 
   struct RClass *plugin_klass = mrb_define_class(mrb, "Plugin", mrb->object_class);
-  mrb_define_method(mrb, plugin_klass, "initialize", mrb_plugin_init,         MRB_ARGS_REQ(3));
-  mrb_define_method(mrb, plugin_klass, "name",       mrb_plugin_get_name,     MRB_ARGS_NONE());
-  mrb_define_method(mrb, plugin_klass, "protocol",   mrb_plugin_get_protocol, MRB_ARGS_NONE());
-  mrb_define_method(mrb, plugin_klass, "port",       mrb_plugin_get_port,     MRB_ARGS_NONE());
+  mrb_define_method(mrb, plugin_klass, "initialize",   mrb_plugin_init,            MRB_ARGS_REQ(3));
+  mrb_define_method(mrb, plugin_klass, "name",         mrb_plugin_get_name,        MRB_ARGS_NONE());
+  mrb_define_method(mrb, plugin_klass, "filter_name",  mrb_plugin_get_filter_name, MRB_ARGS_NONE());
+  mrb_define_method(mrb, plugin_klass, "protocol",     mrb_plugin_get_protocol,    MRB_ARGS_NONE());
+  mrb_define_method(mrb, plugin_klass, "port",         mrb_plugin_get_port,        MRB_ARGS_NONE());
 
   FILE *plugin_src = fopen("plugin.rb", "r");
   mrb_load_file(mrb, plugin_src);
