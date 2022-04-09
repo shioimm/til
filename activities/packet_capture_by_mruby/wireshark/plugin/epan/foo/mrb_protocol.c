30e0cb7b82cfce7a9b8ca1fc2b653e2e31d78144
Misaki Shioi <shioi.mm@gmail.com>
2022-04-09 22:19:12 +0900

    Completion packet_capture_by_mruby/wireshark/

--- activities/packet_capture_by_mruby/wireshark/plugin/epan/foo/mrb_protocol.c
+++ activities/packet_capture_by_mruby/wireshark/plugin/epan/foo/mrb_protocol.c
@@ -27,8 +27,11 @@ static mrb_value mrb_protocol_init(mrb_state *mrb, mrb_value self)
   mrb_int   port;
   mrb_get_args(mrb, "Sni", &name, &tprotocol, &port);
 
-  if ((tprotocol != mrb_intern_lit(mrb, "tcp")) && (tprotocol != mrb_intern_lit(mrb, "udp"))) {
+  if (tprotocol != mrb_intern_lit(mrb, "tcp")) {
     mrb_raisef(mrb, E_ARGUMENT_ERROR, "invalid tprotocol: %S", mrb_symbol_value(tprotocol));
+  }
+  if (mrb_fixnum(mrb_funcall(mrb, name, "size", 0)) > 100) {
+    mrb_raisef(mrb, E_ARGUMENT_ERROR, "too long name: %S", mrb_symbol_value(mrb_obj_to_sym(mrb, name)));
   }
 
   mrb_iv_set(mrb, self, mrb_intern_lit(mrb, "@name"),        name);
@@ -95,33 +98,15 @@ static mrb_value mrb_protocol_enable(mrb_state *mrb, mrb_value protocol)
 
   mrb_tmp_str = mrb_funcall(mrb, mrb_protocol_get_name(mrb, protocol), "to_s", 0);
   const char *tmp_cstr_name = mrb_string_cstr(mrb, mrb_tmp_str);
-
-  if (sizeof(*tmp_cstr_name) > sizeof(mrb_protocol.name)) {
-    fprintf(stderr, "too long name");
-    exit(1);
-  } else {
-    strcpy(mrb_protocol.name, tmp_cstr_name);
-  }
+  strcpy(mrb_protocol.name, tmp_cstr_name);
 
   mrb_tmp_str = mrb_funcall(mrb, mrb_protocol_get_filter_name(mrb, protocol), "to_s", 0);
   const char *tmp_cstr_filter_name = mrb_string_cstr(mrb, mrb_tmp_str);
-
-  if (sizeof(*tmp_cstr_filter_name) > sizeof(mrb_protocol.filter_name)) {
-    fprintf(stderr, "too long filter name");
-    exit(1);
-  } else {
-    strcpy(mrb_protocol.filter_name, tmp_cstr_filter_name);
-  }
+  strcpy(mrb_protocol.filter_name, tmp_cstr_filter_name);
 
   mrb_tmp_str = mrb_funcall(mrb, mrb_protocol_get_tprotocol(mrb, protocol), "to_s", 0);
   const char *tmp_cstr_tprotocol = mrb_string_cstr(mrb, mrb_tmp_str);
-
-  if (sizeof(*tmp_cstr_tprotocol) > sizeof(mrb_protocol.tprotocol)) {
-    fprintf(stderr, "too long transport protocol");
-    exit(1);
-  } else {
-    strcpy(mrb_protocol.tprotocol, tmp_cstr_tprotocol);
-  }
+  strcpy(mrb_protocol.tprotocol, tmp_cstr_tprotocol);
 
   mrb_protocol.port = (unsigned int)mrb_fixnum(mrb_protocol_get_port(mrb, protocol));
 
