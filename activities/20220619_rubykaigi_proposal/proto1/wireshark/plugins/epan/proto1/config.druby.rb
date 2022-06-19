18ffbef6879408f40b28b4fe1cc6e71aa0f458af
Misaki Shioi <shioi.mm@gmail.com>
2022-06-19 10:13:35 +0900

    Support Args size

--- activities/20220619_rubykaigi_proposal/proto1/wireshark/plugins/epan/proto1/config.druby.rb
+++ activities/20220619_rubykaigi_proposal/proto1/wireshark/plugins/epan/proto1/config.druby.rb
@@ -46,6 +46,12 @@ WSProtocol.configure("dRuby") do
               type:    WSProtocol::FT_STRING,
               display: WSProtocol::BASE_NONE,
               dict:    nil },
+            { name:    :hf_druby_integer,
+              label:   "Value",
+              filter:  "druby.integer",
+              type:    WSProtocol::FT_INT32,
+              display: WSProtocol::BASE_DEC,
+              dict:    nil },
           ]
 
   protocol =  self
@@ -115,6 +121,37 @@ WSProtocol.configure("dRuby") do
                   size:   message_value_size ? message_value_size.hex - 10 : 0,
                   offset: 16,
                   endian: WSDissector::ENC_NA },
+              ]
+      end
+
+      sub("Args size") do
+        args_size_value_size = protocol.packet(29, :gint32, WSDissector::ENC_BIG_ENDIAN)
+        args_size_value      = protocol.packet(36, :gint8)
+
+        def convert_form_to_int(n)
+          n = n ^ 128 - 128
+
+          if n == 0 then 0
+          elsif n >= 4 then n - 5
+          elsif n < -6 then n + 5
+          elsif ( 1.. 3).include? n
+            # WIP
+          elsif (-5..-1).include? n
+            # WIP
+          end
+        end
+
+        items [
+                { header:  :hf_druby_size,
+                  size:    4,
+                  offset:  29,
+                  endian:  WSDissector::ENC_BIG_ENDIAN },
+                { header:  :hf_druby_integer,
+                  size:    args_size_value_size ? args_size_value_size.hex - 3 : 0,
+                  offset:  36,
+                  display: :formatted_int,
+                  format: "%d",
+                  value:   convert_form_to_int(args_size_value.to_i) },
               ]
       end
     end
