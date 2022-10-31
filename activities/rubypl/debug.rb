53a0771cf7bbc0709d0f5c0cfe6510afad570a8c
Misaki Shioi <shioi.mm@gmail.com>
2022-10-31 18:49:31 +0900

    Update rubypp/

--- activities/rubypl/debug.rb
+++
@@ -1,9 +0,0 @@
-File.open(ARGV[0], "r+") do |f|
-  f.flock(File::LOCK_EX)
-  f.rewind;
-  body = f.read;
-  body = body.gsub("++", "+= 1")
-  f.rewind;
-  f.puts body; # 別ファイルを作って実行し、削除するようにする
-  f.truncate(f.tell);
-end
