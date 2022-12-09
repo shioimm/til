1dd5b966581e07825f2c7af0611226bcfae12532
Misaki Shioi <shioi.mm@gmail.com>
2022-12-09 19:01:19 +0900

    Update rubypp/parsing+=.rb

--- activities/rubypp/parsing+=.rb
+++ activities/rubypp/parsing+=.rb
@@ -64,9 +64,9 @@ i += 1
 # lhs '='
 # ----------------------------
 #
-# Entering state 343                                          stmtへの還元を開始 (-> lex_ctxt mrhsが必要)
+# Entering state 343                                          noneへの還元を開始
 # Stack now 0 2 87 343
-# Reducing stack by rule 781 (line 5914):                     -> ($default) noneを還元 / (none) -> State 577
+# Reducing stack by rule 781 (line 5914):                     -> ($default) none : / (none) -> State 577
 # -> $$ = nterm none (1.3-1.3: )                              noneへの還元を完了
 #
 # ----- semantic stack -------
@@ -256,278 +256,234 @@ i += 1
 # top_stmts terms tIDENTIFIER
 # ----------------------------
 #
-# Entering state 35                                           --WIP--
+# Entering state 35                                           user_variableへの還元を開始 (-> tOP_ASGNが必要)
 # Stack now 0 2 71 313 35
 # Reading a token                                             トークンを読み込み
 # lex_state: END|LABEL -> BEG at line 9835                    parser_yylex() case '='
 # Next token is token "operator-assignment" (2.2-2.4: +)      次のトークンはtOP_ASGN (+=)
-# Reducing stack by rule 652 (line 5193):                     -> user_variableを還元 (tIDENTIFIER)
+# Reducing stack by rule 652 (line 5193):                     -> (tOP_ASGN) user_variable : tIDENTIFIER
 #    $1 = token "local variable or method" (2.0-2.1: i)
-# -> $$ = nterm user_variable (2.0-2.1: )
+# -> $$ = nterm user_variable (2.0-2.1: )                     user_variableへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# user_variable
+# top_stmts terms user_variable
 # ----------------------------
 #
-# Entering state 122
+# Entering state 122                                          var_lhsへの還元を開始 (-> tOP_ASGNが必要)
 # Stack now 0 2 71 313 122
 # Next token is token "operator-assignment" (2.2-2.4: +)      次のトークンはtOP_ASGN (+=)
-# Reducing stack by rule 664 (line 5229):                     -> var_lhsを還元 (user_variable)
+# Reducing stack by rule 664 (line 5229):                     -> (tOP_ASGN) var_lhs : user_variable
 #    $1 = nterm user_variable (2.0-2.1: )
-# -> $$ = nterm var_lhs (2.0-2.1: NODE_LASGN)
+# -> $$ = nterm var_lhs (2.0-2.1: NODE_LASGN)                 var_lhsへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs
+# top_stmts terms var_lhs
 # ----------------------------
 #
-# Entering state 125
-# Stack now 0 2 71 313 125
+# Entering state 125                                          argへの還元を開始
+# Stack now 0 2 71 313 125                                    (-> tOP_ASGN lex_ctxt command_rhsが必要)
 # Next token is token "operator-assignment" (2.2-2.4: +)      次のトークンはtOP_ASGN (+=)
-# Shifting token "operator-assignment" (2.2-2.4: +)           -> tOP_ASGN (+=) をシフト
+# Shifting token "operator-assignment" (2.2-2.4: +)           -> (tOP_ASGN) トークンをシフト
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN
+# top_stmts terms var_lhs tOP_ASGN
 # ----------------------------
 #
-# Entering state 429
+# Entering state 429                                          noneへの還元を開始
 # Stack now 0 2 71 313 125 429
-# Reducing stack by rule 781 (line 5914):                     -> noneを還元 (空)
-# -> $$ = nterm none (2.4-2.4: )
+# Reducing stack by rule 781 (line 5914):                     -> ($default) none : / (none) State 577
+# -> $$ = nterm none (2.4-2.4: )                              noneへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN none
+# top_stmts terms var_lhs tOP_ASGN none
 # ----------------------------
 #
-# Entering state 577
+# Entering state 577                                          lex_ctxtへの還元を開始
 # Stack now 0 2 71 313 125 429 577
-# Reducing stack by rule 270 (line 2739):                     -> lex_ctxtを還元 (none)
+# Reducing stack by rule 270 (line 2739):                     -> ($default) lex_ctxt : none
 #    $1 = nterm none (2.4-2.4: )
-# -> $$ = nterm lex_ctxt (2.4-2.4: )
+# -> $$ = nterm lex_ctxt (2.4-2.4: )                          lex_ctxtへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN lex_ctxt
+# top_stmts terms var_lhs tOP_ASGN lex_ctxt
 # ----------------------------
 #
-# Entering state 658
+# Entering state 658                                          simple_numericへの還元を開始 (-> tINTEGERが必要)
 # Stack now 0 2 71 313 125 429 658
 # Reading a token                                             トークンを読み込み
 # lex_state: BEG -> END at line 8679                          parse_numeric()
 # lex_state: END -> END at line 7973                          set_number_literal()
 # Next token is token "integer literal" (2.5-2.6: 1)          次のトークンはtINTEGER (1)
-# Shifting token "integer literal" (2.5-2.6: 1)               -> tINTEGER (1) をシフト
+# Shifting token "integer literal" (2.5-2.6: 1)               -> (tINTEGER) トークンをシフト / State 41
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN lex_ctxt tINTEGER
+# top_stmts terms var_lhs tOP_ASGN lex_ctxt tINTEGER
 # ----------------------------
 #
-# Entering state 41
+# Entering state 41                                           simple_numericへの還元を開始
 # Stack now 0 2 71 313 125 429 658 41
-# Reducing stack by rule 645 (line 5182):                     -> simple_numericを還元 (tINTEGER)
+# Reducing stack by rule 645 (line 5182):                     -> ($default) simple_numeric : tINTEGER
 #    $1 = token "integer literal" (2.5-2.6: 1)
-# -> $$ = nterm simple_numeric (2.5-2.6: NODE_LIT)
+# -> $$ = nterm simple_numeric (2.5-2.6: NODE_LIT)            simple_numericへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN lex_ctxt simple_numeric
+# top_stmts terms var_lhs tOP_ASGN lex_ctxt simple_numeric
 # ----------------------------
 #
-# Entering state 120
+# Entering state 120                                          numericへの還元を開始
 # Stack now 0 2 71 313 125 429 658 120
-# Reducing stack by rule 643 (line 5171):                     -> numericを還元 (simple_numeric)
+# Reducing stack by rule 643 (line 5171):                     -> ($default) numeric : simple_numeric
 #    $1 = nterm simple_numeric (2.5-2.6: NODE_LIT)
-# -> $$ = nterm numeric (2.5-2.6: NODE_LIT)
+# -> $$ = nterm numeric (2.5-2.6: NODE_LIT)                   numericへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN lex_ctxt numeric
+# top_stmts terms var_lhs tOP_ASGN lex_ctxt numeric
 # ----------------------------
 #
-# Entering state 119
+# Entering state 119                                          literalへの還元を開始
 # Stack now 0 2 71 313 125 429 658 119
-# Reducing stack by rule 595 (line 4797):                     -> literalを還元 (numeric)
+# Reducing stack by rule 595 (line 4797):                     -> ($default) literal : numeric
 #    $1 = nterm numeric (2.5-2.6: NODE_LIT)
-# -> $$ = nterm literal (2.5-2.6: NODE_LIT)
+# -> $$ = nterm literal (2.5-2.6: NODE_LIT)                   literalへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN lex_ctxt literal
+# top_stmts terms var_lhs tOP_ASGN lex_ctxt literal
 # ----------------------------
 #
-# Entering state 106
+# Entering state 106                                          primaryへの還元を開始
 # Stack now 0 2 71 313 125 429 658 106
-# Reducing stack by rule 310 (line 3028):                     -> primaryを還元 (literal)
+# Reducing stack by rule 310 (line 3028):                     -> ($default) primary : literal
 #    $1 = nterm literal (2.5-2.6: NODE_LIT)
-# -> $$ = nterm primary (2.5-2.6: NODE_LIT)
+# -> $$ = nterm primary (2.5-2.6: NODE_LIT)                   primaryへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN lex_ctxt primary
+# top_stmts terms var_lhs tOP_ASGN lex_ctxt primary
 # ----------------------------
 #
-# Entering state 90
+# Entering state 90                                           argへの還元を開始 (-> '\n'が必要)
 # Stack now 0 2 71 313 125 429 658 90
 # Reading a token                                             トークンを読み込み
 # lex_state: END -> BEG at line 9565                          parser_yylex() case '-1'
 # Next token is token '\n' (2.6-2.6: )                        次のトークンは '\n'
-# Reducing stack by rule 263 (line 2716):                     -> argを還元 (primary)
+# Reducing stack by rule 263 (line 2716):                     -> ('\n') arg : primary
 #    $1 = nterm primary (2.5-2.6: NODE_LIT)
-# -> $$ = nterm arg (2.5-2.6: NODE_LIT)
+# -> $$ = nterm arg (2.5-2.6: NODE_LIT)                       argへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN lex_ctxt arg
+# top_stmts terms var_lhs tOP_ASGN lex_ctxt arg
 # ----------------------------
 #
-# Entering state 832
+# Entering state 832                                          arg_rhsへの還元を開始 (-> '\n'が必要)
 # Stack now 0 2 71 313 125 429 658 832
 # Next token is token '\n' (2.6-2.6: )                        次のトークンは'\n'
-# Reducing stack by rule 276 (line 2773):                     -> arg_rhsを還元 (arg   %prec tOP_ASGN)
+# Reducing stack by rule 276 (line 2773):                     -> ('\n') arg_rhs : arg %prec tOP_ASGN
 #    $1 = nterm arg (2.5-2.6: NODE_LIT)
-# -> $$ = nterm arg_rhs (2.5-2.6: NODE_LIT)
+# -> $$ = nterm arg_rhs (2.5-2.6: NODE_LIT)                   arg_rhsへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# var_lhs tOP_ASGN lex_ctxt arg_rhs
+# top_stmts terms var_lhs tOP_ASGN lex_ctxt arg_rhs
 # ----------------------------
 #
-# Entering state 833
+# Entering state 833                                          argへの還元を開始
 # Stack now 0 2 71 313 125 429 658 833
-# Reducing stack by rule 217 (line 2446):                     -> argを還元 (var_lhs tOP_ASGN lex_ctxt arg_rhs)
+# Reducing stack by rule 217 (line 2446):               -> ($default) arg : var_lhs tOP_ASGN lex_ctxt arg_rhs
 #    $1 = nterm var_lhs (2.0-2.1: NODE_LASGN)
 #    $2 = token "operator-assignment" (2.2-2.4: +)
 #    $3 = nterm lex_ctxt (2.4-2.4: )
 #    $4 = nterm arg_rhs (2.5-2.6: NODE_LIT)
-# -> $$ = nterm arg (2.0-2.6: NODE_LASGN)
+# -> $$ = nterm arg (2.0-2.6: NODE_LASGN)                     argへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# arg
+# top_stmts terms arg
 # ----------------------------
 #
-# Entering state 88
+# Entering state 88                                           exprへの還元を開始 (-> '\n'が必要)
 # Stack now 0 2 71 313 88
 # Next token is token '\n' (2.6-2.6: )                        次のトークンは'\n'
-# Reducing stack by rule 65 (line 1861):                      -> exprを還元 (arg %prec tLBRACE_ARG)
+# Reducing stack by rule 65 (line 1861):                      -> ($default) expr : arg %prec tLBRACE_ARG
 #    $1 = nterm arg (2.0-2.6: NODE_LASGN)
-# -> $$ = nterm expr (2.0-2.6: NODE_LASGN)
+# -> $$ = nterm expr (2.0-2.6: NODE_LASGN)                    exprへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# expr
+# top_stmts terms expr
 # ----------------------------
 #
-# Entering state 75
+# Entering state 75                                           stmtへの還元を開始 (-> '\n'が必要)
 # Stack now 0 2 71 313 75
 # Next token is token '\n' (2.6-2.6: )                        次のトークンは'\n'
-# Reducing stack by rule 37 (line 1663):                      -> stmtを還元 (expr)
+# Reducing stack by rule 37 (line 1663):                      -> ($default) stmt : expr
 #    $1 = nterm expr (2.0-2.6: NODE_LASGN)
-# -> $$ = nterm stmt (2.0-2.6: NODE_LASGN)
+# -> $$ = nterm stmt (2.0-2.6: NODE_LASGN)                    stmtへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# stmt
+# top_stmts terms expr
 # ----------------------------
 #
-# Entering state 73
+# Entering state 73                                           top_stmtへの還元を開始 (-> '\n'が必要)
 # Stack now 0 2 71 313 73
 # Next token is token '\n' (2.6-2.6: )                        次のトークンは'\n'
-# Reducing stack by rule 7 (line 1445):                       -> top_stmtを還元 (stmt)
+# Reducing stack by rule 7 (line 1445):                       -> ($default) top_stmt : stmt
 #    $1 = nterm stmt (2.0-2.6: NODE_LASGN)
-# -> $$ = nterm top_stmt (2.0-2.6: NODE_LASGN)
+# -> $$ = nterm top_stmt (2.0-2.6: NODE_LASGN)                top_stmtへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# top_stmt
+# top_stmts terms top_stmt
 # ----------------------------
 #
-# Entering state 520
+# Entering state 520                                          top_stmtsへの還元を開始
 # Stack now 0 2 71 313 520
-# Reducing stack by rule 6 (line 1436):                       -> top_stmtsを還元 (top_stmts terms top_stmt)
+# Reducing stack by rule 6 (line 1436):                    -> ($default) top_stmts : top_stmts terms top_stmt
 #    $1 = nterm top_stmts (1.0-1.5: NODE_LASGN)
 #    $2 = nterm terms (1.5-1.5: )
 #    $3 = nterm top_stmt (2.0-2.6: NODE_LASGN)
-# -> $$ = nterm top_stmts (1.0-2.6: NODE_BLOCK)
+# -> $$ = nterm top_stmts (1.0-2.6: NODE_BLOCK)               top_stmtsへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# top_stmts
+# top_stmts terms top_stmts
 # ----------------------------
 #
-# Entering state 71
+# Entering state 71                                           termへの還元を開始 (-> '\n'が必要)
 # Stack now 0 2 71
-# Next token is token '\n' (2.6-2.6: )
-# Shifting token '\n' (2.6-2.6: )                             '\n'をシフト
+# Next token is token '\n' (2.6-2.6: )                        次のトークンは'\n'
+# Shifting token '\n' (2.6-2.6: )                             -> ('\n') トークンをシフト / State 310
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# top_stmts
-# '\n'
+# top_stmts terms top_stmts '\n'
 # ----------------------------
 #
-# Entering state 310
+# Entering state 310                                          termへの還元を開始
 # Stack now 0 2 71 310
-# Reducing stack by rule 778 (line 5906):                     -> termを還元 ('\n')
+# Reducing stack by rule 778 (line 5906):                     -> ($default) term : '\n'
 #    $1 = token '\n' (2.6-2.6: )
-# -> $$ = nterm term (2.6-2.6: )
+# -> $$ = nterm term (2.6-2.6: )                              termへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# top_stmts
-# term
+# top_stmts terms top_stmts term
 # ----------------------------
 #
-# Entering state 312
+# Entering state 312                                          termsへの還元を開始
 # Stack now 0 2 71 312
-# Reducing stack by rule 779 (line 5909):                     -> termsを還元 (term)
+# Reducing stack by rule 779 (line 5909):                     -> ($default) terms : term
 #    $1 = nterm term (2.6-2.6: )
-# -> $$ = nterm terms (2.6-2.6: )
+# -> $$ = nterm terms (2.6-2.6: )                             termsへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# top_stmts terms
+# top_stmts terms top_stmts terms
 # ----------------------------
 #
-# Entering state 313
+# Entering state 313                                          opt_termsへの還元を開始 (-> "end-of-input"が必要)
 # Stack now 0 2 71 313
 # Reading a token                                             トークンを読み込み
-# Now at end of input.                                        インプットなし
-# Reducing stack by rule 769 (line 5885):                     -> opt_termsを還元 (terms)
+# Now at end of input.                                        次のトークンは"end-of-input"
+# Reducing stack by rule 769 (line 5885):                     -> ("end-of-input") opt_terms : terms
 #    $1 = nterm terms (2.6-2.6: )
-# -> $$ = nterm opt_terms (2.6-2.6: )
+# -> $$ = nterm opt_terms (2.6-2.6: )                         opt_termsへの還元を完了
 #
 # ----- semantic stack -------
-# top_stmts
-# terms
-# top_stmts opt_terms
+# top_stmts terms top_stmts opt_terms
 # ----------------------------
 #
-# Entering state 311
+# Entering state 311                                          --WIP--
 # Stack now 0 2 71 311
 # Reducing stack by rule 3 (line 1416):                       -> top_compstmtを還元 (top_stmts opt_terms)
 #    $1 = nterm top_stmts (1.0-2.6: NODE_BLOCK)
