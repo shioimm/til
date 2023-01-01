c85a41928dec3b5921988121b7efbcff62df2635
Misaki Shioi <shioi.mm@gmail.com>
2023-01-01 12:35:51 +0900

    Update repl to eval program

--- practices/interpreterbook/src/03/repl/repl.go
+++ practices/interpreterbook/src/03/repl/repl.go
@@ -6,6 +6,7 @@ import (
   "io"
   "monkey/lexer"
   "monkey/token"
+  "monkey/evaluator"
 )
 
 const PROMPT = ">> "
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
       continue
     }
 
-    io.WriteString(out, program.String())
-    io.WriteString(out, "\n")
+    evaluated := evaluator.Eval(program)
+    if evaluated != nil {
+      io.WriteString(out, evaluated.Inspect())
+      io.WriteString(out, "\n")
+    }
   }
 }
 
