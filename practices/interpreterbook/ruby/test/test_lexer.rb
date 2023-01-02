28bc410bc017c6330e06319c4f75c1ba463fa256
Misaki Shioi <shioi.mm@gmail.com>
2023-01-02 21:28:53 +0900

    Complete test

--- practices/interpreterbook/ruby/test/test_lexer.rb
+++ practices/interpreterbook/ruby/test/test_lexer.rb
@@ -11,7 +11,19 @@ class TestLexer < MiniTest::Unit::TestCase
              let add = fn(x, y) {
                x + y;
              };
-             let result = add(one, two);"
+             let result = add(one, two);
+
+             !-/*1;
+             1 < 2 > 1;
+
+             if (1 < 2) {
+               return true
+             } else {
+               return false
+             }
+
+             1 == 1
+             1 != 2"
 
     tests = [
       { type: Token::LET,       literal: "let" },
@@ -52,6 +64,48 @@ class TestLexer < MiniTest::Unit::TestCase
       { type: Token::COMMA,     literal: "," },
       { type: Token::IDENT,     literal: "two" },
       { type: Token::RPAREN,    literal: ")" },
+      { type: Token::SEMICOLON, literal: ";" },
+
+      { type: Token::BANG,      literal: "!" },
+      { type: Token::MINUS,     literal: "-" },
+      { type: Token::SLASH,     literal: "/" },
+      { type: Token::ASTERISK,  literal: "*" },
+      { type: Token::INT,       literal: "1" },
+      { type: Token::SEMICOLON, literal: ";" },
+
+      { type: Token::INT,       literal: "1" },
+      { type: Token::LT,        literal: "<" },
+      { type: Token::INT,       literal: "2" },
+      { type: Token::GT,        literal: ">" },
+      { type: Token::INT,       literal: "1" },
+      { type: Token::SEMICOLON, literal: ";" },
+
+      { type: Token::IF,        literal: "if" },
+      { type: Token::LPAREN,    literal: "(" },
+      { type: Token::INT,       literal: "1" },
+      { type: Token::LT,        literal: "<" },
+      { type: Token::INT,       literal: "2" },
+      { type: Token::RPAREN,    literal: ")" },
+      { type: Token::LBRACE,    literal: "{" },
+      { type: Token::RETURN,    literal: "return" },
+      { type: Token::TRUE,      literal: "true" },
+      { type: Token::SEMICOLON, literal: ";" },
+      { type: Token::RBRACE,    literal: "}" },
+      { type: Token::ELSE,      literal: "else" },
+      { type: Token::LBRACE,    literal: "{" },
+      { type: Token::RETURN,    literal: "return" },
+      { type: Token::FALSE,     literal: "false" },
+      { type: Token::SEMICOLON, literal: ";" },
+      { type: Token::RBRACE,    literal: "}" },
+
+      { type: Token::INT,       literal: "1" },
+      { type: Token::EQ,        literal: "==" },
+      { type: Token::INT,       literal: "1" },
+      { type: Token::SEMICOLON, literal: ";" },
+
+      { type: Token::INT,       literal: "1" },
+      { type: Token::NOT_EQ,    literal: "!=" },
+      { type: Token::INT,       literal: "2" },
       { type: Token::SEMICOLON, literal: ";" },
 
       { type: Token::EOF,       literal: "" },
