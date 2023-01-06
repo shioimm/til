e710bb65d36ac2d4882cb2f4397632c35cff5287
Misaki Shioi <shioi.mm@gmail.com>
2023-01-07 08:56:55 +0900

    Add ObjectSystem

---
+++ practices/interpreterbook/ruby/lib/object_system.rb
@@ -0,0 +1,47 @@
+module ObjectSystem
+  INTEGER_OBJ = "INTEGER"
+  BOOLEAN_OBJ = "BOOLEAN"
+  NULL_OBJ    = "NULL"
+
+  class IntegerObject
+    attr_accessor :value
+
+    def initialize(value: nil)
+      @value = value
+    end
+
+    def object_type
+      INTEGER_OBJ
+    end
+
+    def inspect
+      @value.to_s
+    end
+  end
+
+  class BooleanObject
+    attr_accessor :value
+
+    def initialize(value: nil)
+      @value = value
+    end
+
+    def object_type
+      BOOLEAN_OBJ
+    end
+
+    def inspect
+      @value.to_s
+    end
+  end
+
+  class NullObject
+    def object_type
+      NULL_OBJ
+    end
+
+    def inspect
+      "null"
+    end
+  end
+end
