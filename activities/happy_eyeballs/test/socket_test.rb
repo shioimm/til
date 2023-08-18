cdea79f9da18bb179092da95e88d91af6a9bb67e
Misaki Shioi <shioi.mm@gmail.com>
2023-08-18 20:44:12 +0900

    Add test

---
+++ activities/happy_eyeballs/test/socket_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+
+class SocketTest < Minitest::Test
+  def test_that_is_running
+    assert_equal(
+      true,
+      true
+    )
+  end
+end
