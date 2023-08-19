b642a929b4ff420b6996e1a1873a2a52fd4434f0
Misaki Shioi <shioi.mm@gmail.com>
2023-08-19 09:26:29 +0900

    Add socket.rb

---
+++ activities/happy_eyeballs/socket.rb
@@ -0,0 +1,7 @@
+require 'socket'
+
+class Socket
+  def self.tcp(host, port, local_host = nil, local_port = nil, connect_timeout: nil)
+    p :ok
+  end
+end
--- activities/happy_eyeballs/test/socket_test.rb
+++ activities/happy_eyeballs/test/socket_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
-require "socket"
+require_relative "../socket"
 
 class SocketTest < Minitest::Test
   def test_that_returns_IPv6_connected_socket_when_IPv4_address_name_resolution_takes_time
