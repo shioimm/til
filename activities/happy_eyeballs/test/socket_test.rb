52c708c0c469e56e86e5474a84c024c772b979c6
Misaki Shioi <shioi.mm@gmail.com>
2023-08-20 10:28:32 +0900

    Update test

--- activities/happy_eyeballs/test/socket_test.rb
+++ activities/happy_eyeballs/test/socket_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "minitest/autorun"
+# require "socket"
 require_relative "../socket"
 
 class SocketTest < Minitest::Test
@@ -84,13 +85,44 @@ class SocketTest < Minitest::Test
     )
   end
 
-  def test_that_raises_ETIMEDOUT_with_resolv_timeout
-    Addrinfo.define_singleton_method(:getaddrinfo) {|*arg| sleep }
-
-    sock = nil
-
-    assert_raise(Errno::ETIMEDOUT) do
-      sock = Socket.tcp("localhost", 9, resolv_timeout: 0.1)
+  def test_that_returns_IPv4_connected_socket_when_IPv6_address_name_resolution_takes_time_and_IPv6_address_connecting_takes_more_time
+    begin
+      ipv6_server = Socket.new(:PF_INET6, :STREAM)
+      ipv6_sockaddr = Socket.pack_sockaddr_in(0, "::1")
+      ipv6_server.bind(ipv6_sockaddr)
+    rescue Errno::EADDRNOTAVAIL # IPv6 is not supported
+      exit
     end
+
+    _, port, = ipv6_server.connect_address.ip_port
+    ipv4_server = TCPServer.new("127.0.0.1", port)
+
+    Addrinfo.define_singleton_method(:getaddrinfo) do |_, _, family, *_|
+      if family == :PF_INET6
+        sleep 0.025
+        [Addrinfo.tcp("::1", port)]
+      else
+        [Addrinfo.tcp("127.0.0.1", port)]
+      end
+    end
+
+    ipv4_server_thread = Thread.new { ipv4_server.accept }
+    connected_socket = Socket.tcp("localhost", port)
+    ipv4_server_thread.join
+
+    assert_equal(
+      connected_socket.remote_address.ipv4?,
+      true
+    )
   end
+
+ def test_that_raises_ETIMEDOUT_with_resolv_timeout
+   Addrinfo.define_singleton_method(:getaddrinfo) {|*arg| sleep }
+
+   sock = nil
+
+   assert_raises(Errno::ETIMEDOUT) do
+     sock = Socket.tcp("localhost", 9, resolv_timeout: 0.1)
+   end
+ end
 end
