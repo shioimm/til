2cb6148ccc648c7a179d1dbb170b80a1bfcc563b
Misaki Shioi <shioi.mm@gmail.com>
2023-12-15 20:25:35 +0900

    Update benchmark script

--- activities/happy_eyeballs/server.rb
+++ activities/happy_eyeballs/server.rb
@@ -67,10 +67,10 @@ class Server
 end
 
 if child_pid = fork
-  #Server.new(:ipv4, wait: 60 * 60 * 24).accept_loop
+  # Server.new(:ipv4, wait: 60 * 60 * 24).accept_loop
   Server.new(:ipv4, wait: 0).accept_loop
   Process.waitpid(child_pid)
 else
-  Server.new(:ipv6, wait: 60 * 60 * 24).accept_loop
-  #Server.new(:ipv6, wait: 0).accept_loop
+  # Server.new(:ipv6, wait: 60 * 60 * 24).accept_loop
+  Server.new(:ipv6, wait: 0).accept_loop
 end
