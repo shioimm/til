f262b5b951a2cb9edcc885226901f8cefb2043d5
Misaki Shioi <shioi.mm@gmail.com>
2024-01-23 20:33:16 +0900

    Update benchmark.rb

--- activities/happy_eyeballs/Socket_tcp/performance/benchmark.rb
+++ activities/happy_eyeballs/Socket_tcp/performance/benchmark.rb
@@ -1,6 +1,47 @@
 require 'socket'
 require 'benchmark'
 
+HOSTNAME = "www.ruby-lang.org"
+PORT = 80
+
+ai = Addrinfo.tcp(HOSTNAME, PORT)
+
+Benchmark.bmbm do |x|
+  x.report("Domain name") do
+    30.times { Socket.tcp(HOSTNAME, PORT).close }
+  end
+
+  x.report("IP Address") do
+    30.times { Socket.tcp(ai.ip_address, PORT).close }
+  end
+
+  x.report("fast_fallback: false") do
+    30.times { Socket.tcp(HOSTNAME, PORT, fast_fallback: false).close }
+  end
+end
+
+# ~/s/build ❯❯❯ ../install/bin/ruby ../ruby/test.rb
+# Rehearsal --------------------------------------------------------
+# Domain name            0.012003   0.021894   0.033897 (  0.418290)
+# IP Address             0.006983   0.021459   0.028442 (  0.339960)
+# fast_fallback: false   0.006970   0.027291   0.034261 (  0.463858)
+# ----------------------------------------------- total: 0.096600sec
+#
+#                            user     system      total        real
+# Domain name            0.014590   0.026427   0.041017 (  0.420891)
+# IP Address             0.005251   0.016003   0.021254 (  1.371125)
+# fast_fallback: false   0.007465   0.029140   0.036605 (  0.366034)
+#
+# ~/s/build ❯❯❯ ruby ../ruby/test.rb
+# Rehearsal --------------------------------------------
+# Ruby 3.3   0.004308   0.016682   0.020990 (  1.496522)
+# ----------------------------------- total: 0.020990sec
+#
+#                user     system      total        real
+# Ruby 3.3   0.007271   0.027410   0.034681 (  0.472510)
+
+__END__
+
 HOSTNAME = "localhost"
 PORT = 9292
 n = 10_000
