3777aaa77fba78a22c096d4a3379debaba0b8e36
Misaki Shioi <shioi.mm@gmail.com>
2024-04-18 23:48:49 +0900

    Add client

---
+++ activities/happy_eyeballs/Socket_tcp/rubykaigi2024/client.rb
@@ -0,0 +1,9 @@
+require 'socket'
+
+HOSTNAME = "localhost"
+PORT = 4567
+
+Socket.tcp(HOSTNAME, PORT) do |socket|
+  socket.write "Hi\r\n"
+  print socket.read
+end
--- activities/happy_eyeballs/Socket_tcp/rubykaigi2024/server.rb
+++ activities/happy_eyeballs/Socket_tcp/rubykaigi2024/server.rb
@@ -2,28 +2,28 @@ require 'socket'
 
 class Server
   ADDRESS_FAMILIES = {
-    ipv6: [Socket::AF_INET6, "::1"],
-    ipv4: [Socket::AF_INET,  "127.0.0.1"],
+    IPv6: [Socket::AF_INET6, "::1"],
+    IPv4: [Socket::AF_INET,  "127.0.0.1"],
   }
 
   def initialize(version)
     @version = version
     family, address = ADDRESS_FAMILIES[version]
     @socket  = Socket.new(family, :STREAM)
-    sockaddr = Socket.pack_sockaddr_in(9292, address)
+    sockaddr = Socket.pack_sockaddr_in(4567, address)
     @socket.bind(sockaddr)
     @socket.setsockopt(:SOCKET, :REUSEADDR, true)
     trap(:INT) { @socket.close; exit }
   end
 
   def accept_loop
-    puts "#{@version.capitalize} server started"
-    sleep if @version == :ipv6
+    puts "#{@version} server started"
+    sleep 5 if @version == :ipv6
     @socket.listen(5)
 
     loop do
       connection, addrinfo = @socket.accept
-      puts "Received #{@version.capitalize} request"
+      puts "Received #{@version} request"
       connection.readpartial(128)
       connection.write("Connection OK: #{addrinfo.ip_address}\n")
       connection.close
@@ -34,8 +34,8 @@ class Server
 end
 
 if child_pid = fork
-  Server.new(:ipv4).accept_loop
+  Server.new(:IPv6).accept_loop
   Process.waitpid(child_pid)
 else
-  Server.new(:ipv6).accept_loop
+  Server.new(:IPv4).accept_loop
 end
