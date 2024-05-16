01680f148b358da55c95644985bd1af4751c6507
Misaki Shioi <shioi.mm@gmail.com>
2024-05-16 09:07:43 +0900

    Add resistor_color.rb

---
+++ sandbox/exercism/ruby/2024/resistor_color.rb
@@ -0,0 +1,9 @@
+# https://exercism.org/tracks/ruby/exercises/resistor-color
+
+class ResistorColor
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
+
+  def self.color_code(color)
+    COLORS.index(color)
+  end
+end
