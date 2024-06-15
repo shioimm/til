0889f9e9aa8befcb054ae2d2a107b8a91fa793e7
Misaki Shioi <shioi.mm@gmail.com>
2024-06-15 12:02:59 +0900

    Add TODOs

--- activities/happy_eyeballs/Socket_tcp/v6.rb
+++ activities/happy_eyeballs/Socket_tcp/v6.rb
@@ -48,7 +48,7 @@ class Socket
     connected_socket = nil
     is_windows_environment ||= (RUBY_PLATFORM =~ /mswin|mingw|cygwin/)
 
-    resources = {
+    resources = { # TODO 不要になる予定
       hostname_resolution_threads: hostname_resolution_threads,
       hostname_resolution_queue: hostname_resolution_queue,
       connecting_sockets: connecting_sockets,
@@ -102,7 +102,7 @@ class Socket
             puts "[DEBUG] #{count}: Socket for #{writable_socket.remote_address.ip_address} is connected" if DEBUG
             connected_socket = writable_socket
             connecting_sockets.delete connected_socket
-            cleanup_resources(**resources)
+            cleanup_resources(**resources) # TODO 不要になる予定
             break
           else
             failed_ai = connecting_sockets.delete writable_socket
@@ -118,7 +118,7 @@ class Socket
             else
               ip_address = failed_ai.ipv6? ? "[#{failed_ai.ip_address}]" : failed_ai.ip_address
               last_error = SystemCallError.new("connect(2) for #{ip_address}:#{failed_ai.ip_port}", sockopt.int)
-              cleanup_resources(**resources)
+              cleanup_resources(**resources) # TODO 不要になる予定
               raise last_error
             end
           end
@@ -136,7 +136,7 @@ class Socket
            connecting_sockets.empty? &&
            resolved_addrinfos.empty? &&
            !hostname_resolved)
-        cleanup_resources(**resources)
+        cleanup_resources(**resources) # TODO 不要になる予定
         raise Errno::ETIMEDOUT, 'user specified timeout'
       end
 
@@ -198,7 +198,7 @@ class Socket
                 # Exit this "while" and wait for hostname resolution in next loop
                 break
               else
-                cleanup_resources(**resources)
+                cleanup_resources(**resources) # TODO 不要になる予定
                 raise SocketError.new 'no appropriate local address'
               end
             end
@@ -225,7 +225,7 @@ class Socket
             case result
             when 0, Socket
               connected_socket = socket
-              cleanup_resources(**resources)
+              cleanup_resources(**resources) # TODO 不要になる予定
               break
             when :wait_writable # 接続試行中
               connecting_sockets.add(socket, addrinfo)
@@ -244,7 +244,7 @@ class Socket
               ends_at = hostname_resolution_expires_at if resolv_timeout
               # Exit this "while" and wait for hostname resolution in next loop
             else
-              cleanup_resources(**resources)
+              cleanup_resources(**resources) # TODO 不要になる予定
               raise last_error
             end
           end
@@ -259,12 +259,14 @@ class Socket
       if resolved_addrinfos.empty? &&
           connecting_sockets.empty? &&
           hostname_resolution_queue.closed?
-        cleanup_resources(**resources)
+        cleanup_resources(**resources) # TODO 不要になる予定
         raise last_error
       end
       puts "------------------------" if DEBUG
     end
 
+    # TODO ここでまとめて後処理
+    # TODO 以下tcp_without_fast_fallbackと共通化する
     puts "[DEBUG] ret.remote_address #{ret.remote_address.ip_address}" if DEBUG
     if block_given?
       begin
