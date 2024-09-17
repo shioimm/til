7d3033dda3fe7e39e82b1236884ecedf5e4492de
Misaki Shioi <shioi.mm@gmail.com>
2024-09-17 18:54:17 +0900

    Get benchmark

--- activities/happy_eyeballs/TCPSocket_new/performance/benchmark00.rb
+++ activities/happy_eyeballs/TCPSocket_new/performance/benchmark00.rb
@@ -4,37 +4,26 @@ require 'benchmark'
 HOSTNAME = "localhost"
 PORT = 9292
 
-n = 100
-ai = Addrinfo.tcp(HOSTNAME, PORT)
+n = 1000
 
 Benchmark.bmbm do |x|
-  x.report("Domain name") do
+  x.report("fast_fallback: true") do
     n.times { TCPSocket.new(HOSTNAME, PORT).close }
   end
 
-  x.report("IP Address") do
-    n.times { TCPSocket.new(ai.ip_address, PORT).close }
+  x.report("fast_fallback: false") do
+    n.times { TCPSocket.new(HOSTNAME, PORT, fast_fallback: false).close }
   end
 end
 
 __END__
 
-~/w/ruby ❯❯❯ ruby ../ruby/test.rb
-Rehearsal -----------------------------------------------
-Domain name   0.000485   0.002446   0.002931 (  0.005548)
-IP Address    0.000231   0.001079   0.001310 (  0.002120)
--------------------------------------- total: 0.004241sec
+~/w/build ❯❯❯ ../install/bin/ruby ../ruby/test.rb
+Rehearsal --------------------------------------------------------
+fast_fallback: true    0.027206   0.141390   0.168596 (  0.279012)
+fast_fallback: false   0.018718   0.094518   0.113236 (  0.237073)
+----------------------------------------------- total: 0.281832sec
 
-                  user     system      total        real
-Domain name   0.000326   0.001877   0.002203 (  0.004425)
-IP Address    0.000180   0.000988   0.001168 (  0.002017)
-
-~/w/ruby ❯❯❯ ../install/bin/ruby ../ruby/test.rb
-Rehearsal -----------------------------------------------
-Domain name   0.000610   0.003283   0.003893 (  0.004886)
-IP Address    0.000253   0.001121   0.001374 (  0.002028)
--------------------------------------- total: 0.005267sec
-
-                  user     system      total        real
-Domain name   0.000523   0.002791   0.003314 (  0.004849)
-IP Address    0.000212   0.001094   0.001306 (  0.001927)
+                           user     system      total        real
+fast_fallback: true    0.028090   0.147802   0.175892 (  0.249219)
+fast_fallback: false   0.018436   0.096178   0.114614 (  0.246049)
