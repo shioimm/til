e834b38a47f22e2c95b2fee8e9f6ba431bf91731
Misaki Shioi <shioi.mm@gmail.com>
2024-12-08 11:07:21 +0900

    Improve docs

--- activities/happy_eyeballs/Socket_tcp/doc.rb
+++ activities/happy_eyeballs/Socket_tcp/doc.rb
@@ -3,14 +3,19 @@ class Socket < BasicSocket
   #   Socket.tcp(host, port, local_host=nil, local_port=nil, [opts]) {|socket| ... }
   #   Socket.tcp(host, port, local_host=nil, local_port=nil, [opts])
   #
-  # creates a new socket object connected to host:port using TCP/IP.
+  # Creates a new socket object connected to host:port using TCP/IP.
   #
   # Starting from Ruby 3.4, this method operates according to the
   # Happy Eyeballs Version 2 ({RFC 8305}[https://datatracker.ietf.org/doc/html/rfc8305])
-  # algorithm by default.
+  # algorithm by default, except on Windows.
+  #
+  # For details on Happy Eyeballs Version 2, please refer to the documentation of
+  # {Socket.tcp_fast_fallback=}[rdoc-ref:Socket#tcp_fast_fallback=].
   #
   # To make it behave the same as in Ruby 3.3 and earlier,
-  # explicitly specify the option +fast_fallback:false+.
+  # explicitly specify the option fast_fallback:false.
+  # Or, setting Socket.tcp_fast_fallback=false will disable
+  # Happy Eyeballs Version 2 not only for this method but for all Socket globally.
   #
   # If local_host:local_port is given,
   # the socket is bound to it.
@@ -33,24 +38,6 @@ class Socket < BasicSocket
   #     sock.close_write
   #     puts sock.read
   #   }
-  #
-  # === Happy Eyeballs Version 2
-  # Happy Eyeballs Version 2 ({RFC 8305}[https://datatracker.ietf.org/doc/html/rfc8305])
-  # is an algorithm designed to improve client socket connectivity.<br>
-  # It aims for more reliable and efficient connections by performing hostname resolution
-  # and connection attempts in parallel, instead of serially.
-  #
-  # Starting from Ruby 3.4, this method operates as follows with this algorithm:
-  #
-  # 1. Start resolving both IPv6 and IPv4 addresses concurrently.
-  # 2. Start connecting to the one of the addresses that are obtained first.<br>If IPv4 addresses are obtained first,
-  #    the method waits 50 ms for IPv6 name resolution to prioritize IPv6 connections.
-  # 3. After starting a connection attempt, wait 250 ms for the connection to be established.<br>
-  #    If no connection is established within this time, a new connection is started every 250 ms<br>
-  #    until a connection is  established or there are no more candidate addresses.<br>
-  #    (Although RFC 8305 strictly specifies sorting addresses,<br>
-  #    this method only alternates between IPv6 / IPv4 addresses due to the performance concerns)
-  # 4. Once a connection is established, all remaining connection attempts are canceled.
   def self.tcp(host, port, local_host = nil, local_port = nil, connect_timeout: nil, resolv_timeout: nil, fast_fallback: tcp_fast_fallback, &) # :yield: socket
     # ...
   end
