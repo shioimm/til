6d1feb085f04897a6f4048837724d9a692a9d7ff
Misaki Shioi <shioi.mm@gmail.com>
2025-02-07 11:37:31 +0900

    Update issues/getsockopt_win.rb

--- ruby/dev/issues/getsockopt_win.rb
+++ ruby/dev/issues/getsockopt_win.rb
@@ -6,9 +6,35 @@ sock.connect_nonblock(addr, exception: false)
 _, _, excepts = IO.select(nil, nil, [sock], 5)
 code = excepts.last.getsockopt(Socket::SOL_SOCKET, Socket::SO_ERROR).int
 
-SystemCallError.new("Error", error_code)
+SystemCallError.new("Error", code)
 
 __END__
+# Windows 10 Version 22H2
+require "socket"
+
+sock = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+addr = Socket.sockaddr_in(9999, "127.0.0.1")
+sock.connect_nonblock(addr, exception: false)
+_, _, excepts = IO.select(nil, nil, [sock], 5)
+code = excepts.last.getsockopt(Socket::SOL_SOCKET, Socket::SO_ERROR).int
+
+SystemCallError.new("Error", code)
+# => #<SystemCallError: No connection could be made because the target machine actively refused id. - connect(2) for 127.0.0.1:50306>
+
+TCPSocket.new("127.0.0.1", 9999)
+# => No connection could be made because the target machine actively refused id. - connect(2) for "127.0.0.1" port 9999 (Errno::ECONNREFUSED)
+
+# macOS / Ubuntu
+require "socket"
+sock = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+addr = Socket.sockaddr_in(9999, "127.0.0.1")
+sock.connect_nonblock(addr, exception: false)
+_, writables, = IO.select(nil, [sock], nil, 5)
+code = writables.last.getsockopt(Socket::SOL_SOCKET, Socket::SO_ERROR).int
+
+SystemCallError.new("Error", code)
+# => #<Errno::ECONNREFUSED: Connection refused - Error>
+
 - ext/socket/basicsocket.c
   - bsock_getsockopt
 - ext/socket/option.c
