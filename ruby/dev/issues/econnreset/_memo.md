023f3dd0fe680440338bcafd6ea3f7b28fcb315a
Misaki Shioi <shioi.mm@gmail.com>
2025-02-22 09:38:46 +0900

    Update issues/econnreset/

---
+++ ruby/dev/issues/econnreset/_memo.md
@@ -0,0 +1,7 @@
+#### IPv6を無効 / 有効化 (macOS)
+
+```
+$ sudo networksetup -setv6off Wi-Fi
+$ networksetup -getinfo Wi-Fi # 確認
+$ sudo networksetup -setv6automatic Wi-Fi
+```
--- ruby/dev/issues/econnreset/openssl.rb
+++ ruby/dev/issues/econnreset/openssl.rb
@@ -5,8 +5,7 @@ hostname = "ruby-lang.org"
 port = 443
 
 tcp_socket = TCPSocket.new(hostname, port)
-ssl_context = OpenSSL::SSL::SSLContext.new
-ssl_socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, ssl_context)
+ssl_socket = OpenSSL::SSL::SSLSocket.new(tcp_socket, OpenSSL::SSL::SSLContext.new)
 ssl_socket.connect
 
 puts "Connected to #{hostname}:#{port} using #{ssl_socket.ssl_version}"
