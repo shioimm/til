556530f91da0b5cf44afbaf8e6bde017519daec3
Misaki Shioi <shioi.mm@gmail.com>
2025-04-11 09:34:09 +0900

    Update net/httpx.rb

--- ruby/dev/net/httpx.rb
+++ ruby/dev/net/httpx.rb
@@ -25,3 +25,95 @@ p res.status
 p res.headers["last-modified"]
 __END__
 - res, res.headersともに無名クラスのインスタンスになっている
+
+# https://github.com/HoneyryderChuck/httpx/blob/master/lib/httpx/chainable.rb
+
+%w[head get post put delete trace options connect patch].each do |meth|
+  class_eval(<<-MOD, __FILE__, __LINE__ + 1)
+    def #{meth}(*uri, **options)                # def get(*uri, **options)
+      request("#{meth.upcase}", uri, **options) #   request("GET", uri, **options)
+    end                                         # end
+  MOD
+end
+
+def request(*args, **options)
+  branch(default_options).request(*args, **options)
+end
+
+def branch(options, &blk)
+  return self.class.new(options, &blk) if is_a?(S)
+
+  Session.new(options, &blk)
+en
+
+# https://github.com/HoneyryderChuck/httpx/blob/master/lib/httpx/session.rb
+
+def request(*args, **params)
+  raise ArgumentError, "must perform at least one request" if args.empty?
+
+  requests = args.first.is_a?(Request) ? args : build_requests(*args, params)
+  responses = send_requests(*requests)
+  return responses.first if responses.size == 1
+
+  responses
+end
+
+def send_requests(*requests)
+  selector = get_current_selector { Selector.new }
+  begin
+    _send_requests(requests, selector)
+    receive_requests(requests, selector)
+  ensure
+    unless @wrapped
+      if @persistent
+        deactivate(selector)
+      else
+        close(selector)
+      end
+    end
+  end
+end
+
+def send_request(request, selector, options = request.options)
+  error = begin
+    catch(:resolve_error) do
+      connection = find_connection(request.uri, selector, options)
+      connection.send(request)
+    end
+  rescue StandardError => e
+    e
+  end
+  return unless error && error.is_a?(Exception)
+
+  raise error unless error.is_a?(Error)
+
+  response = ErrorResponse.new(request, error)
+  request.response = response
+  request.emit(:response, response)
+end
+
+def find_connection(request_uri, selector, options)
+  if (connection = selector.find_connection(request_uri, options))
+    return connection
+  end
+
+  connection = @pool.checkout_connection(request_uri, options)
+
+  case connection.state
+  when :idle
+    do_init_connection(connection, selector)
+  when :open
+    if options.io
+      select_connection(connection, selector)
+    else
+      pin_connection(connection, selector)
+    end
+  when :closing, :closed
+    connection.idling
+    select_connection(connection, selector)
+  else
+    pin_connection(connection, selector)
+  end
+
+  connection
+end
