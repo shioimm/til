8226f5a928eefd2112fdbed9a2e11049a3bd2d15
Misaki Shioi <shioi.mm@gmail.com>
2025-04-24 18:54:55 +0900

    Add 101

---
+++ sandbox/go/101/server/server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(writer http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(writer, "hello")
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
