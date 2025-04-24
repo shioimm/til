6cd2f282460af55514615b6579f122639c708059
Misaki Shioi <shioi.mm@gmail.com>
2025-04-24 19:06:37 +0900

    Update server.go

--- sandbox/go/101/server/server.go
+++ sandbox/go/101/server/server.go
@@ -1,3 +1,5 @@
+// https://www.kohgakusha.co.jp/books/detail/978-4-7775-2239-2
+
 package main
 
 import (
@@ -9,8 +11,21 @@ func hello(writer http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(writer, "hello")
 }
 
+func algebra(writer http.ResponseWriter, req *http.Request) {
+	a := 1
+	b := 2
+	result := "%d + %d = %d\n"
+	fmt.Fprintf(writer, result, a, b, a + b)
+
+	c := 15.0
+	d := float64(a + b + 4)
+	result = "%.1f / %.1f = %.3f\n"
+	fmt.Fprintf(writer, result, c, d, c / d)
+}
+
 func main() {
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/algebra", algebra)
 	http.ListenAndServe(":8090", nil)
 }
 
