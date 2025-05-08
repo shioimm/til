5be084cc4a9f193075244a9a7fa86b4de6bcc01a
Misaki Shioi <shioi.mm@gmail.com>
2025-05-08 15:39:17 +0900

    Update learning_go/

---
+++ sandbox/go/learning_go/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string = "Hello, ☀️"
+	var b []byte = []byte(s)
+	var r []rune = []rune(s)
+
+	fmt.Println(s) // Hello, ☀️
+	fmt.Println(b) // [72 101 108 108 111 44 32 226 152 128 239 184 143]
+	fmt.Println(r) // [72 101 108 108 111 44 32 9728 65039]
+}
