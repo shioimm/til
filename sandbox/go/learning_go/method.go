09bbb521dacd20bc2bad7864c9fc2654cb9341a3
Misaki Shioi <shioi.mm@gmail.com>
2025-05-15 14:41:10 +0900

    Update method.go

--- sandbox/go/learning_go/method.go
+++ sandbox/go/learning_go/method.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Type struct {
 	Name string
@@ -11,6 +14,30 @@ func (t Type) Pp() string {
 	return fmt.Sprintf("%d: %s", t.No, t.Name)
 }
 
+type Counter struct {
+	total int
+	lastUpdated time.Time
+}
+
+func (c *Counter) Increment() {
+	c.total++
+	c.lastUpdated = time.Now()
+}
+
+func (c Counter) Pp() string {
+	return fmt.Sprintf("%d, updated at %s", c.total, c.lastUpdated)
+}
+
+func doUpdateWrong(c Counter) {
+	c.Increment()
+	fmt.Println("NG ", c.Pp())
+}
+
+func doUpdateRight(c *Counter) {
+	c.Increment()
+	fmt.Println("OK ", c.Pp())
+}
+
 func main() {
 	foo := Type{
 		Name: "Foo",
@@ -18,4 +45,14 @@ func main() {
 	}
 
 	fmt.Println(foo.Pp())
+
+	var c Counter
+	fmt.Println(c.Pp())
+	c.Increment()
+	fmt.Println(c.Pp())
+
+	doUpdateWrong(c)
+	fmt.Println("main ", c.Pp())
+	doUpdateRight(&c)
+	fmt.Println("main ", c.Pp())
 }
