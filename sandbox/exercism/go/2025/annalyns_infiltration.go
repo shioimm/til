795bb3c54dfbb32c720314e5e4aaea92ba27b3a7
Misaki Shioi <shioi.mm@gmail.com>
2025-05-25 11:48:05 +0900

    Add cars_assemble.go

--- sandbox/exercism/go/2025/annalyns_infiltration.go
+++ sandbox/exercism/go/2025/annalyns_infiltration.go
@@ -14,8 +14,6 @@ func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
 	return !archerIsAwake && prisonerIsAwake
 }
 
-// CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
-// or if Annalyn's pet dog is with her and the archer is sleeping.
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
 	return (prisonerIsAwake && !knightIsAwake && !archerIsAwake) || (petDogIsPresent && !archerIsAwake)
 }
