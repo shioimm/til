a673092e886a9cd6c318fe28821a77cf0f7df4da
Misaki Shioi <shioi.mm@gmail.com>
2025-06-07 10:46:06 +0900

    Add type_chessboard.go

---
+++ sandbox/exercism/go/2025/type_chessboard.go
@@ -0,0 +1,31 @@
+// https://exercism.org/tracks/go/exercises/chessboard
+
+package chessboard
+
+type File []bool
+
+type Chessboard map[string]File
+
+func CountInFile(cb Chessboard, file string) int {
+	count := 0
+
+	for _, cell := range cb[file] {
+		if cell { count++ }
+	}
+
+	return count
+}
+
+func CountInRank(cb Chessboard, rank int) int {
+	if rank > 8 || rank < 1 { return 0 }
+
+	count := 0
+
+	for _, row := range cb {
+		for i, cell := range row {
+			if i + 1 == rank && cell { count++ }
+		}
+	}
+
+	return count
+}
