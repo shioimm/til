e9d952681a70aa5b570b1939b0a65bc20616e99a
Misaki Shioi <shioi.mm@gmail.com>
2025-06-23 09:06:00 +0900

    Update 2025/func_expenses.go

--- sandbox/exercism/go/2025/func_expenses.go
+++ sandbox/exercism/go/2025/func_expenses.go
@@ -30,3 +30,20 @@ func ByDaysPeriod(p DaysPeriod) func(Record) bool {
 		return record.Day >= p.From && record.Day <= p.To
 	}
 }
+
+func ByCategory(c string) func(Record) bool {
+	return func(record Record) bool {
+		return record.Category == c
+	}
+}
+
+func TotalByPeriod(in []Record, p DaysPeriod) float64 {
+	total := 0.0
+	filterdRecords := Filter(in, ByDaysPeriod(p))
+
+	for _, record := range filterdRecords {
+		total += record.Amount
+	}
+
+	return total
+}
