d0743129e8e78776456abb2916652106a7ce5360
Misaki Shioi <shioi.mm@gmail.com>
2025-07-17 14:23:11 +0900

    Add real_world_http_3nd/https_server.go

---
+++ sandbox/go/real_world_http_3nd/https_server.go
@@ -0,0 +1,29 @@
+// Real World HTTP 第3版 P210
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	dump, err := httputil.DumpRequest(r, true)
+
+	if err != nil {
+		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(string(dump))
+	fmt.Fprintf(w, "<html><body>Hello</body></html>\n")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Println("start http listening :18443")
+	err := http.ListenAndServeTLS(":18443", "server.crt", "server.key", nil)
+	log.Println(err)
+}
