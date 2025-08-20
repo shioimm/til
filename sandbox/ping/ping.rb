e982eca0b4f67c834a768f8249d1d0156d8bcaae
Misaki Shioi <shioi.mm@gmail.com>
2025-08-20 21:07:40 +0900

    Introduce class Ping

--- sandbox/ping/ping.rb
+++ sandbox/ping/ping.rb
@@ -1,6 +1,16 @@
-def ping(dest)
-  agv = 1 / 1 # WIP
-  puts "RTT (Avg): #{agv}ms"
+class Ping
+  def self.execute!(dest)
+    new(dest).execute!
+  end
+
+  def initialize(dest)
+    @dest = dest
+  end
+
+  def execute!
+    agv = 1 / 1 # WIP
+    puts "RTT (Avg): #{agv}ms"
+  end
 end
 
 dest = ARGV.first
@@ -8,7 +18,7 @@ dest = ARGV.first
 begin
   raise "Missing ping target" if dest.nil?
 
-  ping(dest)
+  Ping.execute!(dest)
 rescue => e
   puts e.full_message
 end
