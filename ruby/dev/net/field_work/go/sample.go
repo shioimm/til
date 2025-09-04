fd077d2991c2a2a0800d467510ec60b7eff0cf4d
Misaki Shioi <shioi.mm@gmail.com>
2025-09-04 15:24:06 +0900

    Add go/sample.go

---
+++ ruby/dev/net/field_work/go/sample.go
@@ -0,0 +1,31 @@
+// https://pkg.go.dev/net/http?utm_source=chatgpt.com
+
+package main
+
+import (
+	"net/http"
+	"io"
+	"fmt"
+)
+
+func main() {
+	req, err := http.NewRequest("GET", "https://example.com", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Accept", "text/html")
+
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	body, _ := io.ReadAll(res.Body)
+	fmt.Println("GET status:", res.Status)
+	fmt.Println("GET body:", string(body))
+}
