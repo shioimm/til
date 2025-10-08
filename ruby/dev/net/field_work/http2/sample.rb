49831ddcaa09ab74f55e1a47c736a34a689a10aa
Misaki Shioi <shioi.mm@gmail.com>
2025-10-08 09:39:21 +0900

    Update http2/sample.rb

--- ruby/dev/net/field_work/http2/sample.rb
+++ ruby/dev/net/field_work/http2/sample.rb
@@ -8,28 +8,31 @@ PORT = 443
 tcp = TCPSocket.new(HOST, PORT)
 
 ctx = OpenSSL::SSL::SSLContext.new
-ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
+ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE # 証明書の検証を無効化
 ctx.alpn_protocols = ["h2"]
 
 tls = OpenSSL::SSL::SSLSocket.new(tcp, ctx)
-tls.sync_close = true
+tls.sync_close = true # tls.close時にTCPもクローズする
 tls.hostname = HOST
 tls.connect
 
+# サーバがh2を選択しなかった場合abort
 if tls.alpn_protocol != "h2"
   abort("ALPN failed (negotiated: #{tls.alpn_protocol.inspect})")
 end
 
 conn = HTTP2::Client.new
-stream = conn.new_stream
+stream = conn.new_stream # ストリームを作成
 body = ""
 finished = false
 
+# フレームが生成されると呼ばれるコールバック
 conn.on(:frame) do
-  tls.write it
-  tls.flush
+  tls.write it # フレームの内容を表すバイト列
+  tls.flush # 即時送信する
 end
 
+# レスポンスヘッダを受信すると呼ばれるコールバック
 stream.on(:headers) do |headers|
   puts "--- HEADER ---"
   headers.each do |k, v|
@@ -37,10 +40,12 @@ stream.on(:headers) do |headers|
   end
 end
 
+# レスポンスボディチャンクを受信すると呼ばれるコールバック
 stream.on(:data) do
-  body << it
+  body << it # レスポンスボディのチャンクをbodyに追加
 end
 
+# このストリームがクローズすると呼ばれるコールバック
 stream.on(:close) do
   finished = true
 end
@@ -53,18 +58,16 @@ headers = {
   "accept"     => "*/*",
 }
 
-stream.headers(headers, end_stream: true)
+# PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n + SETTINGS + HEADERSを送信
+stream.headers(headers, end_stream: true) # end_stream: true = リクエストボディなし (GET)
 
 begin
   until finished
-    r, = IO.select([tls], nil, nil, 30)
-
-    next if r.nil?
-
-    data = tls.readpartial(16 * 1024)
-    conn << data
+    r, = IO.select([tls], nil, nil, nil) # 読み込み可能になるまで待機
+    data = tls.readpartial(16 * 1024) # 受信したバイト列を取得
+    conn << data # 取得したバイト列をHTTP/2パーサへ供給 -> イベントが発火
   end
-rescue EOFError
+rescue EOFError # サーバ側がクローズしたなどの場合
 ensure
   puts "--- BODY ---"
   puts body
