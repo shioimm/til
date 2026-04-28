1ca4c2fddb1793e01d08d68fab42af8e60c0c6fe
Misaki Shioi <shioi.mm@gmail.com>
2026-04-28 13:26:07 +0900

    Add ruby/sockets/addrinfo.rb

---
+++ ruby/sockets/addrinfo.rb
@@ -0,0 +1,79 @@
+# https://www.rubydoc.info/stdlib/socket/4.0.0/Addrinfo
+
+require "socket"
+
+puts "--- Addrinfo.foreach ---"
+Addrinfo.foreach("ruby-lang.org", 80) do |addrinfo|
+  p addrinfo
+end
+puts "\n"
+
+puts "--- Addrinfo.getaddrinfo ---"
+pp Addrinfo.getaddrinfo("ruby-lang.org", 80)
+puts "\n"
+
+puts "--- Addrinfo.ip ---"
+p Addrinfo.ip("ruby-lang.org")
+puts "\n"
+
+puts "--- Addrinfo.tcp ---"
+p Addrinfo.tcp("ruby-lang.org", 80)
+puts "\n"
+
+puts "--- Addrinfo.udp ---"
+p Addrinfo.udp("ruby-lang.org", 80)
+puts "\n"
+
+puts "--- Addrinfo.unix ---"
+p Addrinfo.unix("ruby-lang.org", 80) # #<Addrinfo: UNIX ruby-lang.org SOCK_???(80)>
+puts "\n"
+
+addrinfo = Addrinfo.tcp("ruby-lang.org", 80)
+
+puts "--- Addrinfo#afamily ---"
+p addrinfo.afamily
+puts "\n"
+
+# bind
+# canonname
+# connect(timeout: nil, &block)
+# connect_from(*args, timeout: nil, &block)
+# connect_to(*args, timeout: nil, &block)
+# family_addrinfo(*args)
+# getnameinfo(*args)
+# initialize(*args)
+# inspect
+# inspect_sockaddr
+# ip?
+# ip_address
+# ip_port
+# ip_unpack
+# ipv4?
+# ipv4_loopback?
+# ipv4_multicast?
+# ipv4_private?
+# ipv6?
+# ipv6_linklocal?
+# ipv6_loopback?
+# ipv6_mc_global?
+# ipv6_mc_linklocal?
+# ipv6_mc_nodelocal?
+# ipv6_mc_orglocal?
+# ipv6_mc_sitelocal?
+# ipv6_multicast?
+# ipv6_sitelocal?
+# ipv6_to_ipv4
+# ipv6_unique_local?
+# ipv6_unspecified?
+# ipv6_v4compat?
+# ipv6_v4mapped?
+# listen(backlog = Socket::SOMAXCONN)
+# marshal_dump
+# marshal_load(ary)
+# pfamily
+# protocol
+# socktype
+# to_s
+# to_sockaddr
+# unix?
+# unix_path
