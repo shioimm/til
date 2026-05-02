9aa61d0f5f642fdb3b6323c53fa2d4d697139a28
Misaki Shioi <shioi.mm@gmail.com>
2026-05-03 08:47:14 +0900

    Add BasicSocket#close_read, BasicSocket#close_write, BasicSocket#connect_address, BasicSocket#do_not_reverse_lookup, BasicSocket#do_not_reverse_lookup=(bool), BasicSocket#getpeereid

--- ruby/sockets/basic_socket.rb
+++ ruby/sockets/basic_socket.rb
@@ -18,12 +18,68 @@ server = TCPServer.new("127.0.0.1", 0)
 p BasicSocket.for_fd(server.fileno)
 puts "\n"
 
+r, w = UNIXSocket.pair
+
 puts "--- BasicSocket#close_read ---"
+# static VALUE
+# bsock_close_read(VALUE sock)
+# {
+#     rb_io_t *fptr;
+#
+#     GetOpenFile(sock, fptr);
+#     shutdown(fptr->fd, SHUT_RD);
+#     if (!(fptr->mode & FMODE_WRITABLE)) {
+#         return rb_io_close(sock);
+#     }
+#     fptr->mode &= ~FMODE_READABLE;
+#
+#     return Qnil;
+# }
+p r.close_read
+puts "\n"
+
 puts "--- BasicSocket#close_write ---"
+# static VALUE
+# bsock_close_write(VALUE sock)
+# {
+#     rb_io_t *fptr;
+#
+#     GetOpenFile(sock, fptr);
+#     if (!(fptr->mode & FMODE_READABLE)) {
+#         return rb_io_close(sock);
+#     }
+#     shutdown(fptr->fd, SHUT_WR);
+#     fptr->mode &= ~FMODE_WRITABLE;
+#
+#     return Qnil;
+# }
+
+p w.close_read
+puts "\n"
+
 puts "--- BasicSocket#connect_address ---"
-puts "--- BasicSocket#do_not_reverse_lookup ---"
-puts "--- BasicSocket#do_not_reverse_lookup=(bool) ---"
+# ローカルマシンで接続に適したソケットのアドレスを返す
+p Addrinfo.tcp("0.0.0.0", 0).listen.connect_address
+p Addrinfo.tcp("::", 0).listen.connect_address
+puts "\n"
+
+TCPSocket.open("www.ruby-lang.org", 80) { |sock|
+  puts "--- BasicSocket#do_not_reverse_lookup ---"
+  p sock.do_not_reverse_lookup
+  puts "\n"
+
+  puts "--- BasicSocket#do_not_reverse_lookup=(bool) ---"
+  p sock.do_not_reverse_lookup = !sock.do_not_reverse_lookup
+  puts "\n"
+}
+
 puts "--- BasicSocket#getpeereid ---"
+# rb_undef_method(rb_cIPSocket, "getpeereid");↲されているので、
+# BasicSocketを直接継承しているUNIXSocket (か、UNIXSocketを継承しているUNIXServer) でしか使えない
+r, _w = UNIXSocket.pair
+p r.getpeereid # [_wの実効UID, _wの実効GID]
+puts "\n"
+
 puts "--- BasicSocket#getpeername ---"
 puts "--- BasicSocket#getsockname ---"
 puts "--- BasicSocket#getsockopt(level, optname) ---"
