0ab3f37395c6e3abcb2cd4715a4496a071246c6e
Misaki Shioi <shioi.mm@gmail.com>
2026-07-03 17:26:27 +0900

    Use local DNS / origin

--- ruby/dev/net/hev3/impl01.rb
+++ ruby/dev/net/hev3/impl01.rb
@@ -1,12 +1,24 @@
 require "net/http"
 require "resolv"
 
-HOST = "www.ruby-lang.org"
+NAMESERVER = ["127.0.0.1", 5300]
+HOST = "localhost"
+HTTPS_PORT = 8443
+HTTP_PORT = 8080
 
-resolver = Resolv::DNS.new
+resolver = Resolv::DNS.new(nameserver_port: [NAMESERVER])
 a_records = resolver.getresources(HOST, Resolv::DNS::Resource::IN::A).map { it.address.to_s }
 
-http = Net::HTTP.new(a_records.first)
+http =
+  if ARGV[0] == :https
+    h = Net::HTTP.new(a_records.first, HTTPS_PORT)
+    h.use_ssl = true
+    h.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    h
+  else
+    Net::HTTP.new(a_records.first, HTTP_PORT)
+  end
+
 request = Net::HTTP::Get.new("/")
 request["Host"] = HOST
 
