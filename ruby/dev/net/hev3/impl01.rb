5b50665af62009d4c536ae8437414c20a1529a3d
Misaki Shioi <shioi.mm@gmail.com>
2026-07-04 09:52:20 +0900

    Connect nonblocking

--- ruby/dev/net/hev3/impl01.rb
+++ ruby/dev/net/hev3/impl01.rb
@@ -8,30 +8,54 @@ HTTP_PORT = 8080
 
 resolver = Resolv::DNS.new(nameserver_port: [NAMESERVER])
 a_records = resolver.getresources(HOST, Resolv::DNS::Resource::IN::A).map { it.address.to_s }
-s = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
 
-sock =
-  if ARGV[0] == :https
-    sockaddr = Socket.sockaddr_in(HTTPS_PORT, a_records.first)
-    s.connect(sockaddr)
-    ssl_sock = OpenSSL::SSL::SSLSocket.new(s)
-    ssl_sock.hostname = HOST
-    ssl_sock.connect
-    ssl_sock
-  else
-    sockaddr = Socket.sockaddr_in(HTTP_PORT, a_records.first)
-    s.connect(sockaddr)
-    s
+socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+
+port = ARGV[0] == :https ? HTTPS_PORT : HTTP_PORT
+sockaddr = Socket.sockaddr_in(port, a_records.first)
+socket.connect_nonblock(sockaddr, exception: false)
+
+connecting_sockets = []
+connecting_sockets.push socket
+
+_, writable_sockets, _ = IO.select(
+  nil,
+  connecting_sockets,
+  nil,
+  nil,
+)
+
+while (writable_socket = writable_sockets.pop)
+  is_connected = (
+    sockopt = writable_socket.getsockopt(Socket::SOL_SOCKET, Socket::SO_ERROR)
+    sockopt.int.zero?
+  )
+
+  if is_connected
+    connecting_sockets.delete writable_socket
+
+    if ARGV[0] == :https
+      ssl_socket = OpenSSL::SSL::SSLSocket.new(writable_socket)
+      ssl_socket.hostname = HOST
+      ssl_socket.connect
+      ssl_socket
+      connected_socket = ssl_socket
+    else
+      connected_socket = writable_socket
+    end
+
+    break
   end
+end
 
 request_message = "GET / HTTP/1.1\r\nHost: #{HOST}\r\nConnection: close\r\n\r\n"
-sock.write request_message
+connected_socket.write request_message
 
-response_message = sock.read
+response_message = connected_socket.read
 status_line, *rest = response_message.split("\r\n")
 _, body = rest.join("\r\n").split("\r\n\r\n", 2)
 
 puts status_line
 puts body
 
-sock.close
+connected_socket.close
