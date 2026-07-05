6d75f63aac0a8ea09608da8fa6e6ffd59a7fbfa4
Misaki Shioi <shioi.mm@gmail.com>
2026-07-05 10:39:28 +0900

    Add class

--- ruby/dev/net/hev3/impl01.rb
+++ ruby/dev/net/hev3/impl01.rb
@@ -1,61 +1,75 @@
 require "socket"
 require "resolv"
 
-NAMESERVER = ["127.0.0.1", 5300]
-HOST = "localhost"
-HTTPS_PORT = 8443
-HTTP_PORT = 8080
+class HTTPClient
+  NAMESERVER = ["127.0.0.1", 5300]
+  HOST = "localhost"
+  HTTPS_PORT = 8443
+  HTTP_PORT = 8080
 
-resolver = Resolv::DNS.new(nameserver_port: [NAMESERVER])
-a_records = resolver.getresources(HOST, Resolv::DNS::Resource::IN::A).map { it.address.to_s }
+  def self.run
+    self.new.run
+  end
 
-socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+  def initialize
+    @connecting_sockets = []
+    @connected_socket = nil
+  end
 
-port = ARGV[0] == :https ? HTTPS_PORT : HTTP_PORT
-sockaddr = Socket.sockaddr_in(port, a_records.first)
-socket.connect_nonblock(sockaddr, exception: false)
+  def run
+    resolver = Resolv::DNS.new(nameserver_port: [NAMESERVER])
+    a_records = resolver.getresources(HOST, Resolv::DNS::Resource::IN::A).map { it.address.to_s }
 
-connecting_sockets = []
-connecting_sockets.push socket
+    socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
 
-_, writable_sockets, _ = IO.select(
-  nil,
-  connecting_sockets,
-  nil,
-  nil,
-)
+    port = ARGV[0] == :https ? HTTPS_PORT : HTTP_PORT
+    sockaddr = Socket.sockaddr_in(port, a_records.first)
+    socket.connect_nonblock(sockaddr, exception: false)
 
-while (writable_socket = writable_sockets.pop)
-  is_connected = (
-    sockopt = writable_socket.getsockopt(Socket::SOL_SOCKET, Socket::SO_ERROR)
-    sockopt.int.zero?
-  )
+    @connecting_sockets.push socket
 
-  if is_connected
-    connecting_sockets.delete writable_socket
+    _, writable_sockets, _ = IO.select(
+      nil,
+      @connecting_sockets,
+      nil,
+      nil,
+    )
 
-    if ARGV[0] == :https
-      ssl_socket = OpenSSL::SSL::SSLSocket.new(writable_socket)
-      ssl_socket.hostname = HOST
-      ssl_socket.connect
-      ssl_socket
-      connected_socket = ssl_socket
-    else
-      connected_socket = writable_socket
+    while (writable_socket = writable_sockets.pop)
+      is_connected = (
+        sockopt = writable_socket.getsockopt(Socket::SOL_SOCKET, Socket::SO_ERROR)
+        sockopt.int.zero?
+      )
+
+      if is_connected
+        @connecting_sockets.delete writable_socket
+
+        if ARGV[0] == :https
+          ssl_socket = OpenSSL::SSL::SSLSocket.new(writable_socket)
+          ssl_socket.hostname = HOST
+          ssl_socket.connect
+          ssl_socket
+          @connected_socket = ssl_socket
+        else
+          @connected_socket = writable_socket
+        end
+
+        break
+      end
     end
 
-    break
+    request_message = "GET / HTTP/1.1\r\nHost: #{HOST}\r\nConnection: close\r\n\r\n"
+    @connected_socket.write request_message
+
+    response_message = @connected_socket.read
+    status_line, *rest = response_message.split("\r\n")
+    _, body = rest.join("\r\n").split("\r\n\r\n", 2)
+
+    puts status_line
+    puts body
+  ensure
+    @connected_socket.close
   end
 end
 
-request_message = "GET / HTTP/1.1\r\nHost: #{HOST}\r\nConnection: close\r\n\r\n"
-connected_socket.write request_message
-
-response_message = connected_socket.read
-status_line, *rest = response_message.split("\r\n")
-_, body = rest.join("\r\n").split("\r\n\r\n", 2)
-
-puts status_line
-puts body
-
-connected_socket.close
+HTTPClient.run
