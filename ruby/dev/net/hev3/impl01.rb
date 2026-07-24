fbcc9c079c5b80ec05c05e822fbc8ffd288e4a19
Misaki Shioi <shioi.mm@gmail.com>
2026-07-25 07:06:50 +0900

    Fix: HTTPS RRs with empty supported_records are considered resolved

--- ruby/dev/net/hev3/impl01.rb
+++ ruby/dev/net/hev3/impl01.rb
@@ -321,6 +321,7 @@ class HTTPClient
         end
 
         supported_records = result.records.map { |rr| create_address_candidate_from_rr!(rr) }.compact
+        @errors[HTTPS_TYPE] = nil
         return if supported_records.empty?
 
         sorted_candidates = supported_records.sort_by { |c| c.rr.priority }
